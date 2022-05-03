2cc53d61f8e772f6c910777e7a46f0b30399501c
ChineseWarlord <simonbaks@live.dk>
2022-05-03 10:39:24 +0000

    Added some changes to clienttest.c

--- C socket example + Arduino/Test/ClientTest.c
+++ C socket example + Arduino/Test/ClientTest.c
@@ -12,12 +12,12 @@
 #define MAXBUF 64
 #define PORT 8888
 #define PORT2 8887
-#define IP1 "10.20.0.10"
-#define IP2 "192.168.1.160"
+#define IP1 "10.20.0.16"
+#define IP2 "192.168.1.136"
 
 /* Specify LTE / WiFi interface */
 const char *LTE = "wwan0";
-const char *WiFi = "wlan1";
+const char *WiFi = "wlan0";
 
 /* Misc */
 char message[MAXBUF];
@@ -51,12 +51,27 @@ int main()
 	Client2.sin_addr.s_addr = inet_addr(IP2);
 
 	/* Main running code */
+	int count = 0;
 	while (1)
 	{
 		char TestMsg1[] = "This is LTE";
 		char TestMsg2[] = "This is WiFi";
-		sendto(sockfd1, TestMsg1, sizeof(TestMsg1), 0, (struct sockaddr *)&Client1, len1); // send the data to server
-		sendto(sockfd2, TestMsg2, sizeof(TestMsg2), 0, (struct sockaddr *)&Client2, len2); // send the data to server
+		int test1 = sendto(sockfd1, TestMsg1, sizeof(TestMsg1), 0, (struct sockaddr *)&Client1, len1); // send the data to server
+		if (test1 == sizeof(TestMsg1)){
+			printf("TestMsg1 was successfully sent!\n");
+		}
+		int test2 = sendto(sockfd2, TestMsg2, sizeof(TestMsg2), 0, (struct sockaddr *)&Client2, len2); // send the data to server
+		if (test2 == sizeof(TestMsg2)){
+			printf("TestMsg2 was successfully sent!\n");
+		}
+		count++;
+		printf("Count is: %d\n", count);
+		sleep(1);
+		if(count == 10){
+		close(sockfd1);
+		close(sockfd2);
+		exit(0);
+		}
 	}
 	close(sockfd1 && sockfd2);
 	return 1;
