04cc6ca05011c25f6c37ff57a90884ff9e5cdb3f
ChineseWarlord <ChineseWarlord@github.com>
2022-05-04 13:35:14 +0200

    changes to general

--- C socket example + Arduino/General/ClientTest.c
+++ C socket example + Arduino/General/ClientTest.c
@@ -21,11 +21,22 @@ const char *WiFi = "wlan0";
 /* Misc */
 char message[MAXBUF];
 struct sockaddr_in Client;
+struct sockaddr_in Client2;
 int sockfd, len = sizeof(Client);
+int sockfd2, len2 = sizeof(Client2);
 
 void *sendshit()
 {
-	char TestMsg[] = "Hello does this work? I am testing if it is actually sending this shit to the server via threads";
+	char TestMsg[] = "Client says hello via WiFi!";
+	printf("%s\n", TestMsg);
+	sendto(sockfd, TestMsg, sizeof(TestMsg), 0, (struct sockaddr *)&Client, len); // send the data to server
+	printf("WiFi-Thread id = %ld\n", pthread_self());
+	pthread_exit(NULL);
+}
+
+void *sendshit2()
+{
+	char TestMsg[] = "Client says hello via LTE";
 	printf("%s\n", TestMsg);
 	sendto(sockfd, TestMsg, sizeof(TestMsg), 0, (struct sockaddr *)&Client, len); // send the data to server
 	printf("WiFi-Thread id = %ld\n", pthread_self());
@@ -37,7 +48,9 @@ int main()
 
 	/* Create socket */
 	sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	sockfd2 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, WiFi, strlen(WiFi));
+	setsockopt(sockfd2, SOL_SOCKET, SO_BINDTODEVICE, LTE, strlen(LTE));
 
 	if (sockfd == -1)
 	{
@@ -50,12 +63,17 @@ int main()
 	Client.sin_port = htons(PORT);
 	Client.sin_addr.s_addr = inet_addr(IP);
 
+	Client2.sin_family = AF_INET;
+	Client2.sin_port = htons(PORT);
+	Client2.sin_addr.s_addr = inet_addr(IP2);
+
 	/* Main running code */
-	pthread_t T1;
+	pthread_t T1, T2;
 	while (1)
 	{
 		sleep(1);
 		pthread_create(&T1, NULL, sendshit, NULL);
+		pthread_create(&T2, NULL, sendshit2, NULL);
 	}
 	close(sockfd);
 	return 1;
--- C socket example + Arduino/General/ServerTest.c
+++ C socket example + Arduino/General/ServerTest.c
@@ -18,7 +18,9 @@ const char *WiFi = "wlan0";
 
 /* Misc */
 struct sockaddr_in Server;
+struct sockaddr_in Server2;
 int sockfd, len = sizeof(Server);
+int sockfd2, len2 = sizeof(Server2);
 char Buffer[MAXBUF];
 
 void *receiveshit()
@@ -28,14 +30,23 @@ void *receiveshit()
 	printf("%s\n \n", Buffer);
 }
 
+void *receiveshit2()
+{
+	int rc = recvfrom(sockfd, Buffer, MAXBUF, 0, (struct sockaddr *)&Server, &len);
+	printf("LTE-Thread id = %ld\n", pthread_self());
+	printf("%s\n \n", Buffer);
+}
+
 int main()
 {
 
 	/* Create socket */
 	sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	sockfd2 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, WiFi, strlen(WiFi));
+	setsockopt(sockfd2, SOL_SOCKET, SO_BINDTODEVICE, LTE, strlen(LTE))
 
-	if (sockfd == -1)
+		if (sockfd == -1)
 	{
 		perror("Failed to create socket");
 		exit(0);
@@ -46,8 +57,13 @@ int main()
 	Server.sin_port = htons(PORT);
 	Server.sin_addr.s_addr = INADDR_ANY;
 
+	Server2.sin_family = AF_INET;
+	Server2.sin_port = htons(PORT);
+	Server2.sin_addr.s_addr = INADDR_ANY;
+
 	/* Bind to socket */
 	int b = bind(sockfd, (struct sockaddr *)&Server, sizeof(struct sockaddr));
+	int b2 = bind(sockfd2, (struct sockaddr *)&Server2, sizeof(struct sockaddr));
 	if (b == -1)
 	{
 		perror("Failed to bind");
@@ -56,13 +72,14 @@ int main()
 	}
 
 	/* Main running code */
-	pthread_t T1;
+	pthread_t T1, T2;
 	while (1)
 	{
 		// puts("Emergency exit: CTRL+C");
 		// printf("Waiting for data...\n");
 		sleep(1);
 		pthread_create(&T1, NULL, receiveshit, NULL);
+		pthread_create(&T1, NULL, receiveshit2, NULL);
 
 		/*int rc = recvfrom(sockfd, Buffer, MAXBUF, 0, (struct sockaddr *)&Server, &len);
 		printf("%s\n \n", Buffer);*/
