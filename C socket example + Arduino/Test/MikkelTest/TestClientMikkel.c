1ba1bbf8a41ccfbdd8c1cc08561153ddbac9b9c4
galleg123 <68027526+galleg123@users.noreply.github.com>
2022-05-04 15:53:46 +0200

    Reverted

--- C socket example + Arduino/Test/MikkelTest/TestClientMikkel.c
+++ C socket example + Arduino/Test/MikkelTest/TestClientMikkel.c
@@ -8,23 +8,15 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define PORT1 8888
-#define PORT2 8887
-#define IP1 "10.20.0.16"
-#define IP2 "192.168.1.131"
+#define PORT 8888
+#define IP "192.168.1.45"
 
 
 //SETUP SOCKET
 const char *LTE = "wwan0";
-const char *WiFi = "wlan0";
-struct sockaddr_in ClientLTE;
-struct sockaddr_in ClientWiFi;
-int sockLTE;
-int sockWiFi;
-int lenLTE = sizeof(ClientLTE);
-int lenWiFi = sizeof(ClientWiFi);
-int rc_LTE, rc_WiFi;
-pthread_t T1, T2, T3, T4;
+struct sockaddr_in Client;
+int sockfd1;
+int len = sizeof(Client);
 
 
 int data = 0;
@@ -36,89 +28,25 @@ int generate(int Min, int Max){
   return number;
 }
 
-void Create_Socket_LTE()
-{
-	/* Create socket */
-	sockLTE = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	setsockopt(sockLTE, SOL_SOCKET, SO_BINDTODEVICE, LTE, strlen(LTE));
-
-	if (sockLTE == -1)
-	{
-		perror("Failed to create socket: LTE");
-		exit(0);
-	}
-	else
-	{
-		printf("LTE socket was created successfully\n");
-	}
-
-	/* configure settings to communicate with remote UDP server */
-	ClientLTE.sin_family = AF_INET;
-	ClientLTE.sin_port = htons(PORT1);
-	ClientLTE.sin_addr.s_addr = inet_addr(IP1);
-}
-
-void Create_Socket_WiFi()
-{
-	/* Create socket */
-	sockWiFi = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	setsockopt(sockWiFi, SOL_SOCKET, SO_BINDTODEVICE, WiFi, strlen(WiFi));
-
-	if (sockWiFi == -1)
-	{
-		perror("Failed to create socket: WiFi");
-		exit(0);
-	}
-	else
-	{
-		printf("WiFi socket was successful! \n");
-	}
-
-	/* configure settings to communicate with remote UDP server */
-	ClientWiFi.sin_family = AF_INET;
-	ClientWiFi.sin_port = htons(PORT2);
-	ClientWiFi.sin_addr.s_addr = inet_addr(IP2);
-}
-
-  void *Send_Data_LTE(void *buffer)
-  {
-  	sendto(sockLTE, buffer, sizeof(buffer), 0, (struct sockaddr *)&ClientLTE, lenLTE); // send the data to server
-
-  }
-
-  void *Send_Data_WiFi(void *buffer)
-  {
-  	sendto(sockWiFi, buffer, sizeof(buffer), 0, (struct sockaddr *)&ClientWiFi, lenWiFi); // send the data to server
-
-  }
-
 
 int main() {
   srand(time(NULL));
+  sockfd1 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  setsockopt(sockfd1, SOL_SOCKET, SO_BINDTODEVICE, LTE, strlen(LTE));
 
-  Create_Socket_LTE();
-  Create_Socket_WiFi();
-
-  char TestMsg1[] = "This is LTE something test write this shit";
-	char TestMsg2[] = "This is WiFi something test write this shit";
-
-
+  Client.sin_family = AF_INET;
+  Client.sin_port = htons(PORT);
+  Client.sin_addr.s_addr = inet_addr(IP);
 
   while(1){
-    //data = generate(1, 25000000);
-    //converted_data = htonl(data);
+    data = generate(1, 25000000);
+    converted_data = htonl(data);
 
-    pthread_create(&T3, NULL, Send_Data_LTE, &TestMsg1);
-		pthread_join(T3, NULL);
-    //free(T3);
-		pthread_create(&T4, NULL, Send_Data_WiFi, &TestMsg2);
-		pthread_join(T4, NULL);
-    //free(T4);
-
+    sendto(sockfd1, &converted_data, sizeof(converted_data), 0, (struct sockaddr *)&Client, len);
 
     sleep(2);
   }
-  close(sockLTE && sockWiFi);
+  close(sockfd1);
   return 1;
 
 
