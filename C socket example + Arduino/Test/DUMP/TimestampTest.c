24b5c12908d8a09cfd0a5b8272d2ca068a21a8da
ChineseWarlord <ChineseWarlord@github.com>
2022-05-05 10:32:46 +0200

    Added timestamp testing (which works btw)

--- C socket example + Arduino/Test/DUMP/TimestampTest.c
+++ C socket example + Arduino/Test/DUMP/TimestampTest.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <time.h>
 #include <stdlib.h>
 
+char curr_time[20];
 char *Timestamp()
 {
-    // Timestamp format : [hh:mm:ss dd/mm/yy]
-    char curr_time[1];
+    /* Timestamp format : [hh:mm:ss dd/mm/yy] */
     time_t rawtime;
     struct tm *timeinfo;
     time(&rawtime);
     timeinfo = localtime(&rawtime);
     sprintf(curr_time, "[%d:%d:%d %d/%d/%d]", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900);
-
-    printf("reference &: %s\n", &curr_time);
-    printf("pointer *: %s\n", *curr_time);
-    printf("normal call?: %s\n", curr_time);
 
     return curr_time;
 }
--- C socket example + Arduino/Test/NewClientTest.c
+++ C socket example + Arduino/Test/NewClientTest.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 
 /* Define buffer size, PORT number and server IP */
+const int BUFFER;
 uint PORT1, PORT2;
 const char *IP_LTE;
 const char *IP_WiFi;
@@ -24,11 +25,14 @@ int sockLTE, lenLTE = sizeof(ClientLTE);
 int sockWiFi, lenWiFi = sizeof(ClientWiFi);
 int rc_LTE, rc_WiFi;
 pthread_t T1, T2;
+char curr_time[BUFFER];
 
 void Create_Sockets(uint PORT_LTE, uint PORT_WiFi, const char *IP1, const char *IP2)
 {
+    /* Defining sockets & specifying protocol */
     sockLTE = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     sockWiFi = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    /* Setting socket options & binding interfaces */
     setsockopt(sockLTE, SOL_SOCKET, SO_BINDTODEVICE, LTE, strlen(LTE));
     setsockopt(sockWiFi, SOL_SOCKET, SO_BINDTODEVICE, WiFi, strlen(WiFi));
 
@@ -54,20 +58,34 @@ void Create_Sockets(uint PORT_LTE, uint PORT_WiFi, const char *IP1, const char *
 
 void *sendshit1String()
 {
-    char TestMsg[] = "Client says hello via LTE!";
+    // char TestMsg[] = "Client says hello via LTE!";
     printf("%s\n", TestMsg);
-    sendto(sockLTE, TestMsg, sizeof(TestMsg), 0, (struct sockaddr *)&ClientLTE, lenLTE); // send the data to server
+    sendto(sockLTE, TestMsg, sizeof(TestMsg), 0, (struct sockaddr *)&ClientLTE, lenLTE);
     printf("LTE-Thread id = %ld\n", pthread_self());
     pthread_exit(NULL);
 }
 
-void *sendshit2String()
+void *sendshit2String(char msg)
 {
-    char TestMsg2[] = "Client says hello via WiFi!";
-    printf("%s\n", TestMsg2);
-    sendto(sockWiFi, TestMsg2, sizeof(TestMsg2), 0, (struct sockaddr *)&ClientWiFi, lenWiFi); // send the data to server
+    // char TestMsg2[] = "Client says hello via WiFi!";
+    strcat(curr_time, msg);
+    printf("%s\n", msg);
+    printf("%s\n", curr_time);
+    sendto(sockWiFi, curr_time, sizeof(curr_time), 0, (struct sockaddr *)&ClientWiFi, lenWiFi);
     printf("WiFi-Thread id = %ld\n", pthread_self());
     pthread_exit(NULL);
+}
+
+char *Timestamp()
+{
+    /* Timestamp format : [hh:mm:ss dd/mm/yy] */
+    time_t rawtime;
+    struct tm *timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    sprintf(curr_time, "[%d:%d:%d %d/%d/%d]", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900);
+
+    return curr_time;
 }
 
 /* Main running code */
@@ -78,17 +96,22 @@ int main()
     PORT2 = 9124;
     IP_LTE = "10.20.0.16";
     IP_WiFi = "192.168.1.131";
+    BUFFER = 128;
 
     /* Create sockets */
     Create_Sockets(PORT1, PORT2, IP_LTE, IP_WiFi);
+
+    /* Messages to send */
+    char TestMsg[] = "Client says hello via LTE!";
+    char TestMsg2[] = "Client says hello via WiFi!";
 
     while (1)
     {
         sleep(1);
         pthread_create(&T1, NULL, sendshit1String, NULL);
-        pthread_create(&T2, NULL, sendshit2String, NULL);
+        pthread_create(&T2, NULL, sendshit2String, TestMsg2);
         pthread_join(T1, NULL);
-	    pthread_join(T2, NULL);
+        pthread_join(T2, NULL);
     }
     close(sockLTE && sockWiFi);
     exit(0);
