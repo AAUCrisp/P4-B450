fbe9495038bfcd32186d8d02b350295e05555293
CamillaKusk <70756631+CamillaKusk@users.noreply.github.com>
2022-05-05 10:29:22 +0200

    Update SigTestAlone.c

--- C socket example + Arduino/Control_Unit/SigTestAlone.c
+++ C socket example + Arduino/Control_Unit/SigTestAlone.c
@@ -10,30 +10,29 @@
 
 
 int main() {
-    //int i;
-    //int ii;
-    /*
-    char Buffer[50];
-    int 50;
-    int i = system(iwconfig $_[0]grep Signal| cut -d"=" -f3|cut -d " " -f1);
-    int ii = popen(iwconfig $_[0]grep Signal| cut -d"=" -f3|cut -d " " -f1);
-    if (fgets (Buffer, 50, ii)!=NULL){
-        puts(Buffer);
-        printf("Something happend...");
-    }
-    */
 
     FILE* pipe = popen("iwconfig wlan1 | grep -i --color quality", "r");
     if (pipe){
-        char Buffer1[128];
+        char Buffer[128];
         while(!feof(pipe)){
             if(fgets(Buffer1,128,pipe) !=NULL){}
         }
         pclose(pipe);
-        Buffer1[strlen(Buffer1)-1] = '\0';
-        printf("%s",Buffer1);
+        Buffer[strlen(Buffer1)-1] = '\0';
+        printf("%s",Buffer);
     }
-    //printf("%s",Buffer1);
+
+    FILE* pipe = popen("iwconfig wwan0 | grep -i --color quality", "r");
+    if (pipe){
+        char Buffer[128];
+        while(!feof(pipe)){
+            if(fgets(Buffer,128,pipe) !=NULL){}
+        }
+        pclose(pipe);
+        Buffer[strlen(Buffer1)-1] = '\0';
+        printf("%s",Buffer);
+    }
+    
 
 }
 
