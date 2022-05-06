7b26fd943dc44f708a88576cb7341d1b5714f281
CamillaKusk <70756631+CamillaKusk@users.noreply.github.com>
2022-05-06 09:30:50 +0200

    Update SigTestValGet.c

--- C socket example + Arduino/Test/Camilla/SigTestValGet.c
+++ C socket example + Arduino/Test/Camilla/SigTestValGet.c
@@ -15,26 +15,28 @@ int main() {
     FILE* pipe_WiFi = popen("iwconfig wlan1 | grep -i --color quality", "r");
     if (pipe){
         char BufferWiFi[128];
-        char RSSI[10]= {0};
+        char RSSI[20]= {0};
         while(!feof(pipe_WiFi)){
             if(fgets(BufferWiFi,128,pipe_WiFi) !=NULL){
-                //fseek(pipe_WiFi,0,SEEK_SET);
-                //fread(RSSI,0,4,pipe_WiFi);
+                fseek(pipe_WiFi,0,SEEK_SET);
+                fread(RSSI,0,4,pipe_WiFi);
             }
         }
         pclose(pipe_WiFi);
         BufferWiFi[strlen(BufferWiFi)-1] = '\0';
+        printf(sizeof(BufferWiFi));
         printf("%s\n",BufferWiFi);
         
-        //RSSI[strlen(RSSI)-1] = '\0';
-        //printf("%s\n",RSSI);
+        RSSI[strlen(RSSI)-1] = '\0';
+        printf("%s\n",RSSI);
 
     }
+    /*
     //Get RSRP value from LTE 
     FILE* pipe_LTE = popen("mmcli -m 0 --signal-get", "r");
     if (pipe_LTE){
         char BufferLTE[128];
-        char RSRP[10] = {0};
+        char RSRP[20] = {0};
         while(!feof(pipe_LTE)){
             if(fgets(BufferLTE,128,pipe_LTE) !=NULL){
                 fseek(pipe_LTE,50,SEEK_SET);
@@ -47,4 +49,5 @@ int main() {
         RSRP[strlen(RSRP)-1] = '\0';
         printf("%s\n",RSRP);
     }
+    */
 }
