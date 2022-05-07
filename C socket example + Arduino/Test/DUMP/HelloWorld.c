0da87f3dc84b662de5979e9ed8cc4326c0faf978
ChineseWarlord <simonbaks@live.dk>
2022-05-07 16:26:20 +0200

    Changes

--- C socket example + Arduino/Test/DUMP/HelloWorld.c
+++ C socket example + Arduino/Test/DUMP/HelloWorld.c
@@ -1,25 +1,31 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <time.h>
-#include <stdlib.h>
 #include <sys/types.h>
-#include <errno.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 
-int main(){
-    
+int main() {
     printf("PID of HelloWorld.c: %d\n", getpid());
     int count;
 
-    while (1){
+    while (1) {
         sleep(1);
         count++;
         printf("Hello world! %d\n", count);
+        if (count == 15) {
+            break;
+        }
+        if (count == 15) {
+            break;
+        }
     }
-    //exit(0);
+}
+exit(0);
 }
--- C socket example + Arduino/Test/DUMP/ProcessCreationTest.c
+++ C socket example + Arduino/Test/DUMP/ProcessCreationTest.c
@@ -1,35 +1,35 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <time.h>
-#include <stdlib.h>
 #include <sys/types.h>
-#include <errno.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
 
-
-
-int main()
-{
+int main() {
     pid_t child_pid;
     child_pid = fork();
-    
 
-    if(child_pid == 0){
-    printf("PID of parent_pid: %d\n", getppid());
-    printf("PID of child_pid: %d\n", getpid());
-    char *args[] = {"HelloWorld","c"};
-    execv("./HelloWorld&", args);
-    int count;
-    
-    while (1){
-        count++;
-        printf("Main program/parent process: %d\n", count);
-    }
-    //exit(0);
+    if (child_pid == 0) {
+        printf("PID of parent_pid: %d\n", getppid());
+        printf("PID of child_pid: %d\n", getpid());
+        char *args[] = {"HelloWorld", "c"};
+        execv("./HelloWorld&", args);
+        int count;
+
+        while (1) {
+            sleep(1);
+            count++;
+            printf("Main program/parent process: %d\n", count);
+            if (count == 20) {
+                break;
+            }
+        }
+        exit(0);
     }
 }
