413a0d17598fc56f635dccdc155d61fac02c68ca
root <root@focal-newport.mynet>
2022-05-09 10:17:59 +0000

    Working Signal Monitoring reader

--- C socket example + Arduino/Test/MikkelTest/TestFileRead2.c
+++ C socket example + Arduino/Test/MikkelTest/TestFileRead2.c
@@ -14,40 +14,45 @@ int main() {
     //Get RSSI value from WiFi
     FILE* pipe_WiFi = popen("iwconfig wlan1 | grep -i --color quality", "r");
     if (pipe){
-        char BufferWiFi[128];
-        char RSSI[10]= {0};
-        fseek(pipe_WiFi,45,SEEK_SET);
+        char BufferWiFi[64];
+	int signalWiFi;
+        //char RSSI[60]= {0};
         while(!feof(pipe_WiFi)){
-            if(fgets(BufferWiFi,128,pipe_WiFi) !=NULL){
-                //fseek(pipe_WiFi,0,SEEK_SET);
-                //fread(RSSI,0,4,pipe_WiFi);
-            }
-        }
-        pclose(pipe_WiFi);
-        BufferWiFi[strlen(BufferWiFi)-1] = '\0';
-        printf("%s\n",BufferWiFi);
+		if(fgets(BufferWiFi,64,pipe_WiFi) !=NULL){
+			sscanf(BufferWiFi, "%s %s %s %6c %d", BufferWiFi, BufferWiFi, BufferWiFi, BufferWiFi, &signalWiFi );
+		}
+	}
         
-        RSSI[strlen(RSSI)-1] = '\0';
-        printf("%s\n",RSSI);
+            
+        
+        pclose(pipe_WiFi);
+        printf("Signal for WiFi: %d \n", signalWiFi);
+        
+
+        
+        //RSSI[strlen(RSSI)-1] = '\0';
+        //printf("%s\n",RSSI);
 
     }
+    
+
     //Get RSRP value from LTE 
     FILE* pipe_LTE = popen("mmcli -m 0 --signal-get | grep -i rsrp", "r");
     if (pipe_LTE){
         char BufferLTE[128];
-        char RSRP[10] = {0};
+	int signalLTE;
+        //char RSRP[20] = {0};
         while(!feof(pipe_LTE)){
             if(fgets(BufferLTE,128,pipe_LTE) !=NULL){
-                //fseek(pipe_LTE,50,SEEK_SET);
-                //fread(RSRP,50,55,pipe_LTE);
+                sscanf(BufferLTE, "%s %s %d", BufferLTE, BufferLTE, &signalLTE);
+                
             }
         }
         pclose(pipe_LTE);
-        BufferLTE[strlen(BufferLTE)-1] = '\0';
-        printf("%s",BufferLTE);
-        RSRP[strlen(RSRP)-1] = '\0';
-        printf("%s\n",RSRP);
-    }
-    
+	printf("Signal for LTE: %d \n",signalLTE);
 
+        //RSRP[strlen(RSRP)-1] = '\0';
+        //printf("%s\n",RSRP);
+    }
+   
 }
