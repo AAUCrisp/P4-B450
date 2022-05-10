178fbc05838ff79ca8d3d65514b59f22ecebe87a
galleg123 <mikkelkp360@gmail.com>
2022-05-10 12:31:59 +0200

    Reverted

--- C socket example + Arduino/Test/Simon/Sockets.c
+++ C socket example + Arduino/Test/Simon/Sockets.c
@@ -13,6 +13,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 #include "Sockets_TX_RX.h"
@@ -46,7 +47,6 @@ void Create_Bind_Sockets(Sockets *sock, uint PORT_LTE, uint PORT_WiFi, const cha
     /* Create socket */
     sock->sockLTE = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     sock->sockWiFi = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
     /* Setting up socket options & specifying interface */
     setsockopt(sock->sockLTE, SOL_SOCKET, SO_BINDTODEVICE, LTE, strlen(LTE));
     setsockopt(sock->sockWiFi, SOL_SOCKET, SO_BINDTODEVICE, WiFi, strlen(WiFi));
@@ -88,12 +88,24 @@ void Create_Bind_Sockets(Sockets *sock, uint PORT_LTE, uint PORT_WiFi, const cha
 
 /* Function to receive LTE packets */
 void *receiveLTE(void *socket) {
+    time_struct time1;
+    time_struct time2;
+    int timediff;
+    char voidChar[128];
     Sockets *sock = (Sockets *)socket;
     int LenLTE = sizeof(sock->ServerWiFi);
     RX_LTE = recvfrom(sock->sockLTE, message, BUFFER, 0, (struct sockaddr *)&sock->ServerLTE, &LenLTE);
     printf("LTE-Thread id = %ld\n", pthread_self());
-    printf("%s\n", message);
-    printf("Message from LTE received at: %s\n\n", curr_time);
+    printf("Message from LTE received at: %s\n", curr_time);
+    sscanf(message,"%c %d %c %d %c %d %c %d", voidChar, &time1.hour, voidChar, &time1.minute, voidChar, &time1.second, voidChar, &time1.millis);
+    sscanf(curr_time,"%c %d %c %d %c %d %c %d", voidChar, &time2.hour, voidChar, &time2.minute, voidChar, &time2.second, voidChar, &time2.millis);
+    timediff = (((time1.hour * 3600000) + (time1.minute * 60000) + (time1.second * 1000) + (time1.millis)) - ((time2.hour * 3600000) + (time2.minute * 60000) + (time2.second * 1000) + (time2.millis)) );
+    if (timediff >= 0)
+    {
+        printf("Time difference: %d milliseconds \n\n", timediff);
+    } else{
+        printf("\n");
+    }
     // pthread_exit(NULL);
     receive = malloc(sizeof(receive));
     receive = message;
@@ -103,12 +115,24 @@ void *receiveLTE(void *socket) {
 
 /* Function to receive WiFi packets */
 void *receiveWiFi(void *socket) {
+    time_struct time1;
+    time_struct time2;
+    int timediff;
+    char voidChar[128];
     Sockets *sock = (Sockets *)socket;
     int LenWiFi = sizeof(sock->ServerWiFi);
     RX_WiFi = recvfrom(sock->sockWiFi, message, BUFFER, 0, (struct sockaddr *)&sock->ServerWiFi, &LenWiFi);
     printf("WiFi-Thread id = %ld\n", pthread_self());
-    printf("%s\n", message);
-    printf("Message from WiFi received at: %s\n\n", curr_time);
+    printf("Message from WiFi received at: %s \n", curr_time);
+    sscanf(message,"%c %d %c %d %c %d %c %d", voidChar, &time1.hour, voidChar, &time1.minute, voidChar, &time1.second, voidChar, &time1.millis);
+    sscanf(curr_time,"%c %d %c %d %c %d %c %d", voidChar, &time2.hour, voidChar, &time2.minute, voidChar, &time2.second, voidChar, &time2.millis);
+    timediff = (((time1.hour * 3600000) + (time1.minute * 60000) + (time1.second * 1000) + (time1.millis)) - ((time2.hour * 3600000) + (time2.minute * 60000) + (time2.second * 1000) + (time2.millis)) );
+    if (timediff >= 0)
+    {
+        printf("Time difference: %d milliseconds \n\n", timediff);
+    } else{
+        printf("\n");
+    }
     // pthread_exit(NULL);
     receive = malloc(sizeof(receive));
     receive = message;
@@ -139,11 +163,13 @@ void *transmitWiFi(int data, int sockWiFi) {
 /* Function to timestamp packets */
 char *Timestamp() {
     /* Timestamp format : [hh:mm:ss dd/mm/yy] */
-    time_t rawtime;
     struct tm *timeinfo;
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    sprintf(curr_time, "[%d:%d:%d %d/%d/%d]", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900);
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    timeinfo = localtime(&tv.tv_sec);
+
+
+    sprintf(curr_time, "[%d:%d:%d.%03ld %d/%d/%d]", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec,tv.tv_usec/1000, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900);
 
     return curr_time;
 }
