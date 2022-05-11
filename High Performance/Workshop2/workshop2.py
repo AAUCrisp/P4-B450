c28e9a817f4670bb00a33c376e51f9a2cf13b67b
galleg123 <mikkelkp360@gmail.com>
2022-05-11 16:55:56 +0200

    Removed unused library from JIT TESTING, and added NUMBA JIT til workshop2 for hurtigere compiling

--- High Performance/Workshop2/workshop2.py
+++ High Performance/Workshop2/workshop2.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+from numba import njit
 
 
 def close_figure(event):
@@ -39,35 +40,17 @@ def show(im, text, imgtype=0):
     plt.show()      # Show the GUI
 
 
-if __name__ == '__main__':
-    
-    f = np.asarray(Image.open("AK.png").convert('L'), dtype='int16')        # Opens the image as an array with separate pixels, only with the lumen value, typecasted to int for processing
 
-    pos = 0  # Starting position of conversion
-    size = 256 # How many pixels (from start) to include
-    f = f[pos:pos+size,pos:pos+size]
-    #f.astype(np.int16)
-    f-=128 # Substranct 128 from all the values, to make them center around 0
-    show(f, "Test") # Start GUI and show the result
-    #print(f)
-    blocksize = 8
-    width = int (len(f))
-    height = int (len(f[0]))
-    imageDimensions = [width, height]
-    #print(imageDimensions)
-    blockPartition = np.zeros((int(imageDimensions[0]/blocksize), int(imageDimensions[1]/blocksize), blocksize, blocksize))
-
-    # Partitions the image into blocks of size 8x8
+@njit
+def blockPartitioning(blockPartition, blocksize, f):
     for i in range(len(blockPartition)):
         for ii in range(len(blockPartition[0])):
             for iii in range(blocksize):
                 for iiii in range(blocksize):
                     blockPartition[i][ii][iii][iiii] = f[iii + (blocksize*i)][iiii + (blocksize*ii)]
 
-    #print(blockPartition)               
-    
-    blockPartitionDCT = np.zeros((int(imageDimensions[0]/blocksize), int(imageDimensions[1]/blocksize), blocksize, blocksize))
-    
+@njit
+def blockPartitioningDCT(blockPartitionDCT, blockPartition, blocksize):
     for i in range(len(blockPartitionDCT)):
         for ii in range(len(blockPartitionDCT[0])):
             for iii in range(blocksize):
@@ -80,6 +63,57 @@ if __name__ == '__main__':
                         tempOuter += tempInner * np.cos((np.pi/blocksize)*(n1+1/2)*iii)
                     blockPartitionDCT[i][ii][iii][iiii] = int(tempOuter)
 
+
+
+if __name__ == '__main__':
+    
+    f = np.asarray(Image.open("AK.png").convert('L'), dtype='int16')        # Opens the image as an array with separate pixels, only with the lumen value, typecasted to int for processing
+    """
+    pos = 0  # Starting position of conversion
+    size = 256 # How many pixels (from start) to include
+    f = f[pos:pos+size,pos:pos+size]
+    """
+
+    f-=128 # Substranct 128 from all the values, to make them center around 0
+    show(f, "Test") # Start GUI and show the result
+    #print(f)
+    blocksize = 8
+    width = int (len(f))
+    height = int (len(f[0]))
+    imageDimensions = [width, height]
+    #print(imageDimensions)
+    blockPartition = np.zeros((int(imageDimensions[0]/blocksize), int(imageDimensions[1]/blocksize), blocksize, blocksize))
+
+
+    # Partitions the image into blocks of size 8x8
+    blockPartitioning(blockPartition, blocksize, f)
+
+    """
+    for i in range(len(blockPartition)):
+        for ii in range(len(blockPartition[0])):
+            for iii in range(blocksize):
+                for iiii in range(blocksize):
+                    blockPartition[i][ii][iii][iiii] = f[iii + (blocksize*i)][iiii + (blocksize*ii)]
+    """
+    #print(blockPartition)               
+    
+    blockPartitionDCT = np.zeros((int(imageDimensions[0]/blocksize), int(imageDimensions[1]/blocksize), blocksize, blocksize))
+
+    blockPartitioningDCT(blockPartitionDCT, blockPartition, blocksize)
+
+    """
+    for i in range(len(blockPartitionDCT)):
+        for ii in range(len(blockPartitionDCT[0])):
+            for iii in range(blocksize):
+                for iiii in range(blocksize):
+                    tempOuter = 0
+                    for n1 in range(blocksize):
+                        tempInner = 0
+                        for n2 in range(blocksize):
+                            tempInner += blockPartition[i][ii][n1][n2] * np.cos((np.pi/blocksize)*(n2+1/2)*iiii)
+                        tempOuter += tempInner * np.cos((np.pi/blocksize)*(n1+1/2)*iii)
+                    blockPartitionDCT[i][ii][iii][iiii] = int(tempOuter)
+    """
     
     fDCT = np.zeros((imageDimensions[0], imageDimensions[1]))
     
@@ -87,13 +121,9 @@ if __name__ == '__main__':
 
     
     for i in range(len(blockPartitionDCT)):
-        print(i)
         for ii in range(len(blockPartitionDCT[0])):
-            print(f'i = {i} : ii = {ii}')
             for iii in range(blocksize):
-                print(f'i = {i} : ii = {ii} : iii = {iii}')
                 for iiii in range(blocksize):
-                    print(f'i = {i} : ii = {ii} : iii = {iii} : iiii = {iiii}')
                     fDCT[iii + (blocksize*i)][iiii + (blocksize*ii)] = blockPartitionDCT[i][ii][iii][iiii]
     print(fDCT)
-    show((fDCT),"test")
+    show((fDCT),"test", 1)
