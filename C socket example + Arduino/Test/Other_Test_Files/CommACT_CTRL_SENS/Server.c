61a8ae545394f6241f35712024281a2ddbe13485
Ubeyd-22 <uabuka20@student.aau.dk>
2022-05-12 13:14:20 +0200

    changes to server in this folder

--- C socket example + Arduino/Test/Other_Test_Files/CommACT_CTRL_SENS/Server.c
+++ C socket example + Arduino/Test/Other_Test_Files/CommACT_CTRL_SENS/Server.c
@@ -43,8 +43,8 @@ void *receiveLTE1() {
     RX_LTE1 = recvfrom(sockLTE1, Message, BUFFER, 0, (struct sockaddr *)&ServerLTE1, &lenLTE1);
     printf("LTE-Thread id = %ld\n", pthread_self());
     printf("From LTE1: %s\n", Message);
-    char msg[] = "Hello back from LTE";
-    sendto(sockLTE1, msg, BUFFER, 0, (struct sockaddr *)&ServerLTE1, lenLTE1);
+    //char msg[] = "Hello back from LTE";
+    //sendto(sockLTE1, msg, BUFFER, 0, (struct sockaddr *)&ServerLTE1, lenLTE1);
 
     pthread_exit(NULL);
 }
@@ -54,8 +54,8 @@ void *receiveWiFi1() {
     RX_WiFi1 = recvfrom(sockWiFi1, Message, BUFFER, 0, (struct sockaddr *)&ServerWiFi1, &lenWiFi1);
     printf("WiFi-Thread id = %ld\n", pthread_self());
     printf("From WiFi1: %s\n", Message);
-    char msg[] = "Hello back from WiFi";
-    sendto(sockWiFi1, msg, BUFFER, 0, (struct sockaddr *)&ServerWiFi1, lenWiFi1);
+    //char msg[] = "Hello back from WiFi";
+    //sendto(sockWiFi1, msg, BUFFER, 0, (struct sockaddr *)&ServerWiFi1, lenWiFi1);
     pthread_exit(NULL);
 }
 
@@ -87,8 +87,10 @@ int main() {
     printf("Bind to sockLTE1 was successful\n");
     bindWiFi1 = bind(sockWiFi1, (struct sockaddr *)&ServerWiFi1, sizeof(struct sockaddr));
     printf("Bind to sockWiFi1 was successful\n");
-
+    char msg[] = "Hello back from WiFi";
     while (1) {
+        sendto(sockLTE1, msg, BUFFER, 0, (struct sockaddr *)&ServerLTE1, lenLTE1);
+        sendto(sockWiFi1, msg, BUFFER, 0, (struct sockaddr *)&ServerWiFi1, lenWiFi1);
         pthread_create(&T1, NULL, receiveLTE1, NULL);
         pthread_create(&T2, NULL, receiveWiFi1, NULL);
         // pthread_join(T1, NULL);
