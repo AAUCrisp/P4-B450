410039888f8f4c080fd221a94666fce3b44a9008
ChineseWarlord <ChineseWarlord@github.com>
2022-05-12 09:56:53 +0200

    Update Server.c

--- C socket example + Arduino/Test/Other_Test_Files/CommACT_CTRL_SENS/Server.c
+++ C socket example + Arduino/Test/Other_Test_Files/CommACT_CTRL_SENS/Server.c
@@ -16,10 +16,75 @@
 #include <time.h>
 #include <unistd.h>
 
-int main(){
-    int sockLTE1 = socket(AF_INET,SOCK_DGRAM, IPPROTO_UDP);
-    int sockWiFi1 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+/* Define buffers & PORT number */
+#define BUFFER 128
+char Message[BUFFER];
+uint PORT_LTE1 = 6969;
+uint PORT_WiFi1 = 6968;
+uint PORT_LTE1 = 6967;
+uint PORT_WiFi1 = 6966;
 
-    setsockopt(sockLTE1,SOL_SOCKET, SO_BINDTODEVICE, LTE, strlen(LTE));
-    setsockopt(sockWiFi1,SOL_SOCKET, SO_BINDTODEVICE, LTE, strlen(LTE));
+/* Specify LTE & WiFi interface */
+const char *LTE;
+const char *WiFi;
+
+/* Misc */
+struct sockaddr_in ServerLTE1;
+struct sockaddr_in ServerWiFi1;
+int sockLTE1, lenLTE = sizeof(ServerLTE1);
+int sockWiFi1, lenWiFi = sizeof(ServerWiFi1);
+int bindLTE1, bindWiFi1;
+int RX_LTE1, RX_WiFi1;
+int TX_LTE1, TX_WiFi1;
+pthread_t T1, T2, T3, T4;
+
+/* Function to receive LTE packets */
+void *receiveLTE1() {
+    RX_LTE1 = recvfrom(sockLTE1, Message, BUFFER, 0, (struct sockaddr *)&ServerLTE1, &lenLTE1);
+    printf("LTE-Thread id = %ld\n", pthread_self());
+    printf("From LTE1: %s\n", Message);
+    pthread_exit(NULL);
+}
+
+/* Function to receive WiFi packets */
+void *receiveWiFi1() {
+    RX_WiFi1 = recvfrom(sockWiFi1, Message, BUFFER, 0, (struct sockaddr *)&ServerWiFi1, &lenWiFi1);
+    printf("WiFi-Thread id = %ld\n", pthread_self());
+    printf("From WiFi1: %s\n", Message);
+    pthread_exit(NULL);
+}
+
+int main() {
+    sockLTE1 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    sockWiFi1 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    setsockopt(sockLTE1, SOL_SOCKET, SO_BINDTODEVICE, LTE, strlen(LTE));
+    setsockopt(sockWiFi1, SOL_SOCKET, SO_BINDTODEVICE, LTE, strlen(LTE));
+
+    if (sockLTE1 == -1) {
+        perror("Failed to create sockLTE1");
+        exit(0);
+    }
+    if (sockWiFi1 == -1) {
+        perror("Failed to create sockWiFi1");
+        exit(0);
+    }
+
+    ServerLTE1.sin_family = AF_INET;
+    ServerLTE1.sin_port = htons(PORT_LTE1);
+    ServerLTE1.sin_addr.s_addr = INADDR_ANY;
+
+    ServerWiFi1.sin_family = AF_INET;
+    ServerWiFi1.sin_port = htons(PORT_WiFi1);
+    ServerWiFi1.sin_addr.s_addr = INADDR_ANY;
+
+    bindLTE1 = bind(sockLTE1, (struct sockaddr *)&ServerLTE1, sizeof(struct sockaddr));
+    printf("Bind to sockLTE1 was successful\n");
+    bindWiFi1 = bind(sockWiFi1, (struct sockaddr *)&ServerWiFi1, sizeof(struct sockaddr));
+    printf("Bind to sockWiFi1 was successful\n");
+
+    while (1) {
+        pthread_create(&T1, NULL, receiveLTE1, NULL);
+        pthread_create(&T2, NULL, receiveWiFi1, NULL);
+    }
 }
