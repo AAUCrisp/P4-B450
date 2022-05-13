eae0569100a83a66630b83f6017da8537d285bc7
ChineseWarlord <ChineseWarlord@github.com>
2022-05-13 16:59:58 +0200

    More changes

---
+++ C socket example + Arduino/Sensor/Libraries.c
@@ -0,0 +1,26 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>  //IPC thing
+#include <sys/mman.h>
+#include <sys/shm.h>  //SHM thing
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+
+// Own Libraries
+#include "Headers/Sockets.h"
+// Sockets sock;
+#include "Headers/shm_write_read.h"
+// #include "Headers/RSSI.c"
+
