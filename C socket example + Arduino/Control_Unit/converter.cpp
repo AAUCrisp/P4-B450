50f35691b9f9f516a9599e298a3dfa38510ffa7e
AAUCrisp <stpe19@student.aau.dk>
2022-05-13 17:42:29 +0200

    Things for converter fixed... some of it

--- C socket example + Arduino/Control_Unit/converter.cpp
+++ C socket example + Arduino/Control_Unit/converter.cpp
@@ -1,65 +1,84 @@
-#include <string>
 #include<iostream>
 using namespace std;
-
-
-int main() {
-}
 
 
 // Test Variables
 int data = 250;
 
-
-std::string grid = [];       // Array to contain the grid in the RAM
-int x_axis = 50;      // Length of the x-axis of the grid
-int y_axis = 50;      // Length of the y-axis of the grid
-// int x_axis = 5000;      // Length of the x-axis of the grid
-// int y_axis = 5000;      // Length of the y-axis of the grid
+// -- Setup Area --
+int x_axis = 20;      // Length of the x-axis of the grid
+int y_axis = 20;      // Length of the y-axis of the grid
+int coordinates = x_axis*y_axis;
+string* grid = new string[coordinates]; // Don't forget to delete [] a; when you're done!
 
 
 /* Option to store the grid in memory and save processing power */
 void generate_grid(int x_axis, int y_axis) {
-    printf("\nCreating GRID!\n");
-
     int x_count = 1;
     int y_count = 1;
-    for (int i = 0; i < (x_axis * y_axis); i++) {
-        grid[i] = std::string(x_axis) . ":" . std::string(y_axis);
+    
+    cout << "  --- BUILDING GRID ---  " << endl;
 
-        if (x_count == x_axis){
+    for (int i = 0; i < (x_axis * y_axis); i++) {
+
+        string x = to_string(x_count);
+        string y = to_string(y_count);
+        grid[i] = x + ":" + y;
+        
+
+        if(x_count == (x_axis)){
             x_count = 1;
             y_count++;
         }
+        else {
+            x_count++;
+        }
     }
+    
+    return;
 }
+  
+
+
+
+/* ---------------------------
+--------- NOT DONE!! ---------
+------- - Down Here ------- */
 
 /* Option for processing the coordinates instead of using memory */
-std::string convert_to_grid(int number) {
+string convert_to_coordinate(int number) {
     int x = number % x_axis;
     int y = (number + 1) / x_axis; 
 
-    std::string coordinate = string(x) . ":" . string(y);
+
+    string  str_x = to_string(x);
+    string str_y = to_string(y);
+    string coordinate = str_x + ":" + str_y;
 
     return coordinate;
 }
 
+/* ------- Up Here -----------
+--------- NOT DONE!! ---------
+--------------------------- */
+
 
 int main(){
-   
-   string animals[4] = {"Elephant", "Lion", "Deer", "Tiger"}; //The
-   string type array
-   for (int i = 0; i < 4; i++)
-      cout << animals[i] << endl;
 
-
-
-    printf("\nSensor Data is: %d", data);
+    cout << "\nSensor Data is: " << data << endl;
     generate_grid(x_axis, y_axis);
-    std::string cpu_command = convert_to_grid(data);
-    printf("\nCPU Command is: ");
+
+    for (int i = 0; i < coordinates; i++) {
+        cout << "Sensor Data: " << i+1 << "   Becomes GRID Position   " << grid[i] << endl;      
+
+    }
+
+
+
+    cout << "\nRAM Command is: " << grid[data] << endl;
     //printf(cpu_command);
-    printf("\nRAM Command is: ");
+    string cpu_command = convert_to_coordinate(data);
+    cout << "\nCPU Command is: " << cpu_command << endl;
     // printf(grid[data]);
 
 }
