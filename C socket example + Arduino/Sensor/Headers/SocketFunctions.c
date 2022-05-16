e8dac24b7027400038cfa541952d7f60d00bc9a1
ChineseWarlord <ChineseWarlord@github.com>
2022-05-16 16:14:58 +0200

    Update SocketFunctions.c

--- C socket example + Arduino/Sensor/Headers/SocketFunctions.c
+++ C socket example + Arduino/Sensor/Headers/SocketFunctions.c
@@ -140,7 +140,7 @@ void *transmitLTE(void *socket) {
     Sockets *sock = (Sockets *)socket;
     int LenLTE = sizeof(sock->ClientLTE_TRANSMITTER);
     RAND_INT = (char *)shm_read(10, RAND_KEY);
-    printf("RAND_INT LTE: %s\n" RAND_INT);
+    printf("RAND_INT LTE: %s\n"(char *)RAND_INT);
     TX_LTE = sendto(sock->sockLTE_TRANSMITTER, RAND_INT, BUFFER, 0, (struct sockaddr *)&sock->ClientLTE_TRANSMITTER, LenLTE);
     printf("LTE-Thread id = %ld\n", pthread_self());
     printf("Message from LTE transmitted at: %s\n\n", curr_time);
@@ -153,7 +153,7 @@ void *transmitWiFi(void *socket) {
     Sockets *sock = (Sockets *)socket;
     int LenWiFi = sizeof(sock->ClientLTE_TRANSMITTER);
     RAND_INT = (char *)shm_read(10, RAND_KEY);
-    printf("RAND_INT WiFi: %s\n" RAND_INT);
+    printf("RAND_INT WiFi: %s\n"(char *)RAND_INT);
     TX_WiFi = sendto(sock->sockWiFi_TRANSMITTER, RAND_INT, BUFFER, 0, (struct sockaddr *)&sock->ClientWiFi_TRANSMITTER, LenWiFi);
     printf("WiFi-Thread id = %ld\n", pthread_self());
     // printf("%s\n", GSV);
