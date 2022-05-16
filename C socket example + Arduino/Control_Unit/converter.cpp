98d50e07a94ee27caa895acb34fae7798ba25df4
AAUCrisp <stpe19@student.aau.dk>
2022-05-16 16:58:36 +0200

    FUCK THIS SHIT! Makefile (probably) fixed in Simon folder

--- C socket example + Arduino/Control_Unit/converter.cpp
+++ C socket example + Arduino/Control_Unit/converter.cpp
@@ -11,8 +11,8 @@ using namespace std;
 
 
 // -- Setup Area --
-int x_axis = 20;      // Length of the x-axis of the grid
-int y_axis = 20;      // Length of the y-axis of the grid
+int x_axis = 5000;      // Length of the x-axis of the grid
+int y_axis = 5000;      // Length of the y-axis of the grid
 int coordinates = x_axis*y_axis;
 string* grid = new string[coordinates]; // Don't forget to delete [] a; when you're done!
 
@@ -23,7 +23,7 @@ string int_to_hex(int data, int back = 0) {
 
     stream << hex << data;    // Tell it to convert from int to hex-chars
     string result( stream.str() );      // Do the conversion
-    cout << "Converted to Hex-chars is: " << result << endl;     // Print the result
+    // cout << "Converted to Hex-chars is: " << result << endl;     // Print the result
 
 
     /* -- Coversion back in same object, which already has the value -- */
@@ -37,7 +37,7 @@ string int_to_hex(int data, int back = 0) {
 
 
 /* --- Option to store the grid in memory and save processing power --- */
-void generate_grid(int x_axis, int y_axis) {
+void generate_grid(int x_axis, int y_axis, int hex = 1) {
     int x_count = 1;
     int y_count = 1;
     
@@ -45,11 +45,17 @@ void generate_grid(int x_axis, int y_axis) {
     printf("\n0 Rows Done.");
 
     for (int i = 0; i < (x_axis * y_axis); i++) {
+        string x;
+        string y;
+        if(hex == 1) {
+            x = int_to_hex(x_count);
+            y = int_to_hex(y_count);
+        }
+        else {
+            x = to_string(x_count);
+            y = to_string(y_count);
+        }
 
-        // string x = to_string(x_count);
-        string x = int_to_hex(x_count);
-        // string y = to_string(y_count);
-        string y = int_to_hex(y_count);
         grid[i] = x + ":" + y;      // Write coodinate to grid array
         
 
@@ -73,15 +79,22 @@ void generate_grid(int x_axis, int y_axis) {
   
 
 /* Option for processing the coordinates instead of using memory */
-string convert_to_coordinate(int number) {
-    int x = number % x_axis;
-    int y = ((number) / x_axis)+1; 
+string convert_to_coordinate(int number, int hex=1) {
+    // int x = (number % (x_axis+1));
+    int x = ((number-1) % (x_axis))+1;
+    int y = ((number-1) / x_axis)+1; 
 
+    string str_x;
+    string str_y;
+    if(hex == 1) {
+        str_x = int_to_hex(x);
+        str_y = int_to_hex(y);
+    }
+    else{
+        str_x = to_string(x);
+        str_y = to_string(y);
+    }
 
-    // string  str_x = to_string(x);
-    string  str_x = int_to_hex(x);
-    // string str_y = to_string(y);
-    string str_y = int_to_hex(y);
     string coordinate = str_x + ":" + str_y;
 
     return coordinate;
@@ -92,24 +105,31 @@ int main(int argc, char *argv[]){
     // Test Variables
     int data_int = 9;       // Static Test variable
     string data_str = to_string(data_int);     // For "Firm" data
+    int hex = 1;
 
-    if(argc == 2) {
+    if(argc > 1) {
         cout << "\nArgument accepted." << endl;
         data_str = argv[1];
         data_int = atoi(argv[1]);
+        cout << "Third Argument?: " << argv[3] << endl;
+    }
+    if(argc == 3) {     // For enabling or disabling Hex-char output.
+        cout << "Hex argument received: " << argv[2] << endl;
+        hex = atoi(argv[2]);
     }
 
     else {
         cout << "\nNo arguments inserted, running staticly." << endl;
     }
     
-    generate_grid(x_axis, y_axis);
+    generate_grid(x_axis, y_axis, hex);
     
-    for (int i = 0; i < coordinates; i++) {
-            cout << "Sensor Data: " << i+1 << "   Becomes GRID Position   " << grid[i] << endl;
-        }
+    /* --  Loop for printing out the grid coordinate... TROUBLESHOOTING  --  */
+    // for (int i = 0; i < coordinates; i++) {
+    //         cout << "Sensor Data: " << i+1 << "   Becomes GRID Position   " << grid[i] << endl;
+    // }
 
-    string cpu_command = convert_to_coordinate(data_int);
+    string cpu_command = convert_to_coordinate(data_int, hex);
 
     // -- Print out results -- 
     cout << "\nSensor Data is: " << data_str << endl;
