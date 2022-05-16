5287a8d256331eca2ee8880f64535c993e5fc82e
Ubeyd-22 <uabuka20@student.aau.dk>
2022-05-16 14:37:40 +0200

    s

--- C socket example + Arduino/Actuator/ActuatorServer.c
+++ C socket example + Arduino/Actuator/ActuatorServer.c
@@ -16,6 +16,7 @@ int tx_LTE, tx_WiFI;
 pthread_t T1, T2;
 FILE* in;
 FILE* out;
+int x,y;
 
 char ActuatorBuffer[1024];
 char feedback[1024];
@@ -97,10 +98,10 @@ int initialize_Server() {
 
 }
 
-void logData(char msg[256], int* arr) 
+void logData(char msg[256], struct return) 
 {
     char temp[256];
-    sprintf(temp, "Movement on x and y axis:  x = %d, y = %d \n \n", arr[0], arr[1]);
+    sprintf(temp, "Movement on x and y axis:  x = %d, y = %d \n \n", return.x, return.y);
     fwrite(temp, sizeof(char), strlen(temp), out);
 
 }
@@ -138,9 +139,9 @@ void *ReceiveCoordinateLTE(void *) {
     inet_ntop(remote_addr.ss_family,get_in_addr((struct sockaddr *)&remote_addr), s, sizeof s)); // Prints out the remote sockets address
     //printf("Actuator_LTE: packet is %d bytes long\n", rc_LTE);
     printf("Actuator_LTE: packet contains \"%s\"\n", ActuatorBuffer);
-    
-    int* returnedArr = process_Data(ActuatorBuffer);
-    logData(ActuatorBuffer, returnedArr);
+    struct C;
+    C = process_Data(ActuatorBuffer);
+    logData(ActuatorBuffer, C);
     //printf("Actuator_LTE: robot movement on x-axis: %d\n", returnedArr[0]);
     //printf("Actuator_LTE: robot movement on y-axis: %d\n \n", returnedArr[1]);
     free(returnedArr);
--- C socket example + Arduino/Actuator/processData.cpp
+++ C socket example + Arduino/Actuator/processData.cpp
@@ -4,8 +4,19 @@
 
 //int movement_x;
 //int movement_y;
+
+int update_last_coordinate(int number1, int number2);
+{
+    last_x_coordinate = number1;
+    last_y_coordinate = number2;
+}
+
+
 int * process_Data(char msg[buffer]) {
-    int* result = (int *) malloc(sizeof(int) * 3);
+    struct returnvalues {
+        int x, y;
+    };
+
     bytes_read = sscanf(msg, "%[^:]:%s:%s", tempx, tempy);
     x = hex_to_int(tempx);
     y = hex_to_int(tempy);
@@ -22,9 +33,10 @@ int * process_Data(char msg[buffer]) {
         int movement_y = y - last_y_coordinate;
     }
 
-    result[0] = movement_x;
-    result[1] = movement_y;
-
-    return result;
+    returnvalues.x = movement_x;
+    returnvalues.y = movement_y;
+    update_last_coordinate(x, y);
+    
+    return returnvalues;
 }
 
