d6322387cbd067145b62e83cf94cf55ed325888e
AAUCrisp <stpe19@student.aau.dk>
2022-05-16 13:19:40 +0200

    Converter to Hex and back, functioning

--- C socket example + Arduino/Control_Unit/converter.cpp
+++ C socket example + Arduino/Control_Unit/converter.cpp
@@ -1,11 +1,18 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
 #include<iomanip>
+#include <cstdio>
 #include<iostream>
 using namespace std;
 
 
 // -- Setup Area --
-int x_axis = 50;      // Length of the x-axis of the grid
-int y_axis = 50;      // Length of the y-axis of the grid
+int x_axis = 5;      // Length of the x-axis of the grid
+int y_axis = 5;      // Length of the y-axis of the grid
 int coordinates = x_axis*y_axis;
 string* grid = new string[coordinates]; // Don't forget to delete [] a; when you're done!
 
@@ -16,22 +23,30 @@ void generate_grid(int x_axis, int y_axis) {
     int y_count = 1;
     
     cout << "  --- BUILDING GRID ---  " << endl;
+    printf("\n0 Rows Done.");
 
     for (int i = 0; i < (x_axis * y_axis); i++) {
 
         string x = to_string(x_count);
         string y = to_string(y_count);
-        grid[i] = x + ":" + y;
+        grid[i] = x + ":" + y;      // Write coodinate to grid array
         
 
-        if(x_count == (x_axis)){
-            x_count = 1;
-            y_count++;
+        if(x_count == (x_axis)){        // When the end of the x-axis has been reached
+            printf("\r%d Rows Done", y_count);      // Delete last output line and replace with current process
+            fflush(stdout);     // Forces the print to go through before sleeping
+            // cout << y_count <<  " row done. " << endl;
+            x_count = 1;    // Start at the beginning again
+            y_count++;      // Increase the y-axis coordinate
+            // sleep(1);       // For troubleshooting only, program is too fast i smaller grids
         }
         else {
-            x_count++;
+            x_count++;      // Next x-axis position
         }
     }
+    printf("\rAll Rows Done.\n\n");
+
+
     return;
 }
   
