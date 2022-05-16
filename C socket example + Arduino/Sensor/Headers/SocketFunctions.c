546ce7a941db2c6a652837d1a08e98dd277ce056
ChineseWarlord <ChineseWarlord@github.com>
2022-05-16 15:57:39 +0200

    Update SocketFunctions.c

--- C socket example + Arduino/Sensor/Headers/SocketFunctions.c
+++ C socket example + Arduino/Sensor/Headers/SocketFunctions.c
@@ -114,6 +114,7 @@ void *receiveLTE(void *socket) {
     printf("LTE || LTE-Thread id = %ld\n", pthread_self());
     printf("LTE || Message from LTE received at: %s\n", curr_time);
     printf("LTE || Message: %s\n", message);
+    message = atoi(message);
     shm_write(message, 10, GSV_KEY);
     pthread_exit(NULL);
 }
@@ -127,6 +128,7 @@ void *receiveWiFi(void *socket) {
     printf("WiFi || WiFi-Thread id = %ld\n", pthread_self());
     printf("WiFi || Message from WiFi received at: %s \n", curr_time);
     printf("WiFi || Message: %s\n", message);
+    message = atoi(message);
     shm_write(message, 10, GSV_KEY);
     pthread_exit(NULL);
 }
