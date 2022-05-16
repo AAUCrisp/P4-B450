7b445a957f8550d027b8a314b593b950a97852af
Ubeyd-22 <uabuka20@student.aau.dk>
2022-05-16 13:42:19 +0200

    s

--- C socket example + Arduino/Actuator/processData.h
+++ C socket example + Arduino/Actuator/processData.h
@@ -1,10 +1,11 @@
 #define buffer 1024
+#include <string>
 //char string_coordinates[] = "499:2";
 int bytes_read;
 int x, y;
 int last_x_coordinate;
 int last_y_coordinate;
-char tempx [buffer];
-char tempy [buffer];
+std::string tempx [buffer];
+std::string tempy [buffer];
 int movement_x;
 int movement_y;
