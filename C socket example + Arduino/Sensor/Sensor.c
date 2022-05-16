2bd27b0af3e508828ceee223bef82a65235201cd
ChineseWarlord <ChineseWarlord@github.com>
2022-05-16 09:56:52 +0200

    Changes

--- C socket example + Arduino/Sensor/Sensor.c
+++ C socket example + Arduino/Sensor/Sensor.c
@@ -1,10 +1,24 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>  //IPC thing
+#include <sys/mman.h>
+#include <sys/shm.h>  //SHM thing
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
 #include "Headers/SocketFunctions.h"
 #include "Headers/shm_write_read.h"
-
-#ifndef LIBRARIES
-#define LIBRARIES
-#include "Libraries.c"
-#endif
 
 int generate(int Min, int Max) {
     int number = (rand() % ((Max + 1) - Min)) + Min;
