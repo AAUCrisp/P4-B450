7716fd59108839a31794397f58e29f01dd3168a3
ChineseWarlord <ChineseWarlord@github.com>
2022-05-16 15:22:27 +0200

    Update SocketFunctions.h

--- C socket example + Arduino/Sensor/Headers/SocketFunctions.h
+++ C socket example + Arduino/Sensor/Headers/SocketFunctions.h
@@ -19,7 +19,7 @@ typedef struct _sockets {
 void Sockets_Receiver(Sockets *sock, uint PORT_LTE, uint PORT_WiFi, const char *LTE, const char *WiFi);
 
 /* Function to create transmitter socket */
-void Sockets_Transmitter(Sockets *sock, const char *IP, uint PORT_LTE, uint PORT_WiFi, const char *LTE, const char *WiFi);
+void Sockets_Transmitter(Sockets *sock, const char *IP_LTE, const char *IP_WiFi, uint PORT_LTE, uint PORT_WiFi, const char *LTE, const char *WiFi);
 
 /* Function to receive LTE packets */
 void *receiveLTE(void *socket);
