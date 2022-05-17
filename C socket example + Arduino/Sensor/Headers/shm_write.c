5c521e3196edcd8b3e879309c5c3293e2e138988
ChineseWarlord <ChineseWarlord@github.com>
2022-05-17 11:21:10 +0200

    Update shm_write.c

--- C socket example + Arduino/Sensor/Headers/shm_write.c
+++ C socket example + Arduino/Sensor/Headers/shm_write.c
@@ -36,7 +36,7 @@ void shm_write(int *message, const int SIZE, const char* name) {
     /* write to the shared memory object */
     //sprintf(ptr, "%s", message);
     ptr = message;
-    printf("shm_write passed int: %d\n", ptr);
+    printf("shm_write passed int: %d\n", (int*)ptr);
 
     // printf("This is ptr: %p\n", ptr);
     // printf("This is ptr char*: %s\n", (char*)ptr);
