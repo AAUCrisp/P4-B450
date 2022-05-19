15b53a5781e5386fc11e2ff3a3c9b9d0e0d5c575
ChineseWarlord <ChineseWarlord@github.com>
2022-05-19 15:30:08 +0200

    Update Actuator.cpp

--- P4/Actuator/Actuator_Changes/Actuator.cpp
+++ P4/Actuator/Actuator_Changes/Actuator.cpp
@@ -73,7 +73,7 @@ int main() {
             //COMMANDS = (char*)shm_read(32, COMMANDS_KEY);
             printf("GSV from shared memory: %s\n", COMMANDS);
 
-            snprintf(msg, sizeof(msg), "%s", COMMANDS);
+            //snprintf(msg, sizeof(msg), "%s", COMMANDS);
 
             process_data = processData(COMMANDS);
             logData(process_data);
