820055adeb34efd284d96d11d3c757c28d071ce4
ChineseWarlord <ChineseWarlord@github.com>
2022-05-19 16:36:00 +0200

    :man

--- P4/Sensor/Sensor.c
+++ P4/Sensor/Sensor.c
@@ -70,12 +70,13 @@ int main() {
 
     } else {
         while (1) {
+            usleep(1000);
             msg = shm_read(32, GSV_KEY);
             GSV = atoi(msg);
             printf("GSV from shared memory: %s\n", msg);
             // printf("GSV converted: %d\n", GSV);
             sprintf(buffer, "%d", generate(0, 25000000));
-            
+            usleep(1000);
             shm_write(buffer, 32, RAND_INT_KEY);
 
             if (GSV == B || GSV == L) {
