955ec21cb8905a46351dc28542fbb3e486c24406
AAUCrisp <stpe19@student.aau.dk>
2022-05-19 18:39:29 +0200

    CU Function File :unicorn:

--- P4/Control_Unit/Control_Unit.cpp
+++ P4/Control_Unit/Control_Unit.cpp
@@ -10,6 +10,10 @@
 #define CONVERTER
 #include "converter.cpp"
 #endif
+#ifndef FUNCTIONS
+#define FUNCTIONS
+#include "Headers/Functions.cpp"
+#endif
 // #ifndef SHM_READ
 // #define SHM_READ
 // #include "Headers/shm_read.cpp"
@@ -23,56 +27,7 @@
 int data_int = 1500000;     // Static Test Variable
 // string* grid = new string[coordinates]; // Don't forget to delete [] a; when you're done!
 
-void WiFi_command(Sockets sock) {
-    void* message;
-    char msgDump[32];
-    int data;
-    string coordinate;
-    char* WiFimsg = (char*) malloc(9);
 
-    printf("\n\n==================\nWiFi Listener Started\n==================\n\n");
-    
-    while(1) {
-        message = (void*)receiveWiFi((void*)&sock);
-        cout << "WiFi Receiver || WiFi Actuator Socket: " <<sock.act_WiFi << endl;
-        cout << "WiFi Receiver || LTE Actuator Socket: " <<sock.act_LTE << endl;
-        cout << "WiFi Receiver || Message Parsed from Sockets (data & timestamp) is: " << (const char*)message << endl;
-        sscanf((const char*)message, "%d %[^\n]", &data, msgDump);
-        cout << "WiFi Receiver || Message Parsed from Sockets as INT is: " << data << endl;
-        coordinate = convert_to_coordinate(data);
-        cout << "WiFi Receiver || Coordinate for Actuator is: " << coordinate << "\n\n\n" << endl;
-        // cout << "WiFi Receiver || Size of Coordinate is: " << sizeof(coordinate) << endl;;
-        strcpy(WiFimsg, coordinate.c_str());
-        transmit_command(&sock, WiFimsg);
-        sleep(1);
-    }    
-}
-
-void* LTE_command(void* socket) {
-    // Sockets sock;
-    Sockets *sock = (Sockets *)socket;
-    void* message;
-    char msgDump[32];
-    int data;
-    string coordinate;
-    char* LTEmsg = (char*) malloc(9);
-    
-    printf("\n\n==================\nLTE Listener Started\n==================\n\n");
-    while(1) {
-        message = (void*)receiveLTE((void*)&sock);
-        cout << "LTE Receiver || WiFi Actuator Socket: " <<sock->act_WiFi << endl;
-        cout << "LTE Receiver || LTE Actuator Socket: " <<sock->act_LTE << endl; 
-        cout << "LTE Receiver || Message Parsed from Sockets (data & timestamp) is: " << (const char*)message << endl;
-        sscanf((const char*)message, "%d %[^\n]", &data, msgDump);
-        cout << "LTE Receiver || Message Parsed from Sockets as INT is: " << data << endl;
-        coordinate = convert_to_coordinate(data);
-        cout << "LTE Receiver || Coordinate for Actuator is: " << coordinate << "\n\n\n" << endl;
-        // cout << "LTE Receiver || Size of Coordinate is: " << sizeof(coordinate) << endl;;
-        strcpy(LTEmsg, coordinate.c_str());
-        transmit_command(&sock, LTEmsg);
-        sleep(1);
-    }
-}
 
 
 /* Main running code */
@@ -181,7 +136,7 @@ int main(int argc, char *argv[]) {
         pthread_create(&T1, NULL, LTE_command, (void*)&sock);
         // WiFi_command(sock);
         while(1) {
-            
+
         }
         printf("\nThis shouldn't print!!!\n");
     }
