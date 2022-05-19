8e6fbbd7e18b44f2a74c97fc2bc651fd836aa1c6
ChineseWarlord <ChineseWarlord@github.com>
2022-05-19 15:04:28 +0200

    Update ActuatorFunctions.cpp

--- P4/Actuator/Actuator_Changes/Headers/ActuatorFunctions.cpp
+++ P4/Actuator/Actuator_Changes/Headers/ActuatorFunctions.cpp
@@ -25,7 +25,7 @@
 #include <string>
 using namespace std;
 
-#include "ActuatorFunctions.h"
+//#include "ActuatorFunctions.h"
 #include "hex.cpp"
 
 /* Variables used to create and bind sockets, receive from sockets, and error handle on sockets */
