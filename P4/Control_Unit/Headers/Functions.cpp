7d80537b62bc81a9954527b6e398177be5179bc0
AAUCrisp <stpe19@student.aau.dk>
2022-05-20 11:00:13 +0200

    CU I'm an idiot... maybe :unicorn:

--- P4/Control_Unit/Headers/Functions.cpp
+++ P4/Control_Unit/Headers/Functions.cpp
@@ -8,14 +8,15 @@ void WiFi_command(Sockets sock) {
     printf("\n\n==================\nWiFi Listener Started\n==================\n\n");
     
     while(1) {
+        cout << "WiFi Command Function || WiFi Sensor Socket: " <<sock.sockWiFi_RECEIVER << endl;
         message = (void*)receiveWiFi((void*)&sock);
-        cout << "WiFi Receiver || WiFi Actuator Socket: " <<sock.act_WiFi << endl;
-        cout << "WiFi Receiver || LTE Actuator Socket: " <<sock.act_LTE << endl;
-        cout << "WiFi Receiver || Message Parsed from Sockets (data & timestamp) is: " << (const char*)message << endl;
+        cout << "WiFi Command Function || WiFi Actuator Socket: " <<sock.act_WiFi << endl;
+        cout << "WiFi Command Function || LTE Actuator Socket: " <<sock.act_LTE << endl;
+        cout << "WiFi Command Function || Message Parsed from Sockets (data & timestamp) is: " << (const char*)message << endl;
         sscanf((const char*)message, "%d %[^\n]", &data, msgDump);
-        cout << "WiFi Receiver || Message Parsed from Sockets as INT is: " << data << endl;
+        cout << "WiFi Command Function || Message Parsed from Sockets as INT is: " << data << endl;
         coordinate = convert_to_coordinate(data);
-        cout << "WiFi Receiver || Coordinate for Actuator is: " << coordinate << "\n\n\n" << endl;
+        cout << "WiFi Command Function || Coordinate for Actuator is: " << coordinate << "\n\n\n" << endl;
         // cout << "WiFi Receiver || Size of Coordinate is: " << sizeof(coordinate) << endl;;
         strcpy(WiFimsg, coordinate.c_str());
         transmit_command(&sock, WiFimsg);
@@ -34,14 +35,15 @@ void* LTE_command(void* socket) {
     
     printf("\n\n====================\nLTE Listener Started\n====================\n\n");
     while(1) {
-        cout << "LTE Receiver || WiFi Actuator Socket: " << sock->act_WiFi << endl;
-        cout << "LTE Receiver || LTE Actuator Socket: " << sock->act_LTE << endl; 
-        message = (void*)receiveLTE((void*)&sock);
-        cout << "LTE Receiver || Message Parsed from Sockets (data & timestamp) is: " << (const char*)message << endl;
+        cout << "LTE Command Function || LTE Sensor Socket: " << sock->sockLTE_RECEIVER << endl;
+        message = (void*)receiveLTE((void*)sock);
+        cout << "LTE Command Function || WiFi Actuator Socket: " << sock->act_WiFi << endl;
+        cout << "LTE Command Function || LTE Actuator Socket: " << sock->act_LTE << endl; 
+        cout << "LTE Command Function || Message Parsed from Sockets (data & timestamp) is: " << (const char*)message << endl;
         sscanf((const char*)message, "%d %[^\n]", &data, msgDump);
-        cout << "LTE Receiver || Message Parsed from Sockets as INT is: " << data << endl;
+        cout << "LTE Command Function || Message Parsed from Sockets as INT is: " << data << endl;
         coordinate = convert_to_coordinate(data);
-        cout << "LTE Receiver || Coordinate for Actuator is: " << coordinate << "\n\n\n" << endl;
+        cout << "LTE Command Function || Coordinate for Actuator is: " << coordinate << "\n\n\n" << endl;
         // cout << "LTE Receiver || Size of Coordinate is: " << sizeof(coordinate) << endl;;
         strcpy(LTEmsg, coordinate.c_str());
         transmit_command(sock, LTEmsg);
