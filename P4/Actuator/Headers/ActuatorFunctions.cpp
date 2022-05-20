cf66fb059d79583e4b6ead6ec893c7afc92f64f7
galleg123 <mikkelkp360@gmail.com>
2022-05-20 09:45:34 +0200

    Change

--- P4/Actuator/Headers/ActuatorFunctions.cpp
+++ P4/Actuator/Headers/ActuatorFunctions.cpp
@@ -77,9 +77,9 @@ void processData(char msg[buffer]) {
         printf("movement_x = x - last_x: %d\n", movement_x);
         printf("movement_y = y - last_y: %d\n", movement_y);
     }
-    printf("Movement_x: %d , Movement_y: %d", movement_x, movement_y);
+    printf("Movement_x: %d \n Movement_y: %d \n\n", &movement_x, &movement_y);
     File.open("log.txt", std::ofstream::out | std::ofstream::app);
-    File << "\n\n Movement on the x-axis:" << movement_x << "\n Movement on the y-axis:" << movement_y;
+    File << "\n\n Movement on the x-axis:" << &movement_x << "\n Movement on the y-axis:" << &movement_y;
     File.close();
     
 
