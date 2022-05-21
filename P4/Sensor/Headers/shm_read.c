43159dcec0cc2e2facc1826fc5958a9a8553fbde
ChineseWarlord <ChineseWarlord@github.com>
2022-05-21 20:35:04 +0200

    C

--- P4/Sensor/Headers/shm_read.c
+++ P4/Sensor/Headers/shm_read.c
@@ -29,18 +29,20 @@
 void* shm_read(const int SIZE, const char* name) {
     printf("Do I reach here 1\n");
     /* Semaphore variables */
-    sem_t* SEM_WRITE = sem_init(SEM_WRITE_FNAME, 1, 1);
+    sem_t* SEM_WRITE = sem_init(&SEM_WRITE_FNAME, 1, 1);
     printf("WHY NO WORK? 1\n");
-    printf("SEM_WRITE Semaphore value: %d\n", int sem_getvalue(SEM_WRITE, int* valp));
+    int sem_getvalue1(SEM_WRITE, int* valp);
+    printf("SEM_WRITE Semaphore value: %d\n", sem_getvalue1);
     if (SEM_WRITE == SEM_FAILED) {
         perror("shm_read = sem_open/SEM_WRITE");
         exit(EXIT_FAILURE);
     }
     printf("Do I reach here 2\n");
 
-    sem_t* SEM_READ = sem_init(SEM_READ_FNAME, 1, 1);
+    sem_t* SEM_READ = sem_init(&SEM_READ_FNAME, 1, 1);
     printf("WHY NO WORK? 2\n");
-    printf("SEM_READ Semaphore value: %d\n", int sem_getvalue(SEM_READ, int* valp));
+    int sem_getvalue2(SEM_READ, int* valp);
+    printf("SEM_READ Semaphore value: %d\n", sem_getvalue2);
     if (SEM_READ == SEM_FAILED) {
         perror("shm_read = sem_open/SEM_READ");
         exit(EXIT_FAILURE);
--- P4/Sensor/Headers/shm_write.c
+++ P4/Sensor/Headers/shm_write.c
@@ -32,17 +32,19 @@ void shm_write(const char* message, const int SIZE, const char* name) {
     // sem_unlink(SEM_READ_FNAME);
     // sem_unlink(SEM_WRITE_FNAME);
 
-    sem_t* SEM_WRITE = sem_init(SEM_WRITE_FNAME, 1, 1);
+    sem_t* SEM_WRITE = sem_init(&SEM_WRITE_FNAME, 1, 1);
     printf("WHY NO WORK? 1\n");
-    printf("SEM_WRITE Semaphore value: %d\n", int sem_getvalue(SEM_WRITE, int* valp));
+    int sem_getvalue1(SEM_WRITE, int* valp);
+    printf("SEM_WRITE Semaphore value: %d\n", sem_getvalue1);
     if (SEM_WRITE == SEM_FAILED) {
         perror("shm_write = sem_open/SEM_WRITE");
         exit(EXIT_FAILURE);
     }
     printf("Do I reach here 2\n");
-    sem_t* SEM_READ = sem_init(SEM_READ_FNAME, 1, 1);
+    sem_t* SEM_READ = sem_init(&SEM_READ_FNAME, 1, 1);
     printf("WHY NO WORK? 2\n");
-    printf("SEM_READ Semaphore value: %d\n", int sem_getvalue(SEM_READ, int* valp));
+    int sem_getvalue2(SEM_READ, int* valp);
+    printf("SEM_READ Semaphore value: %d\n", sem_getvalue2);
     if (SEM_READ == SEM_FAILED) {
         perror("shm_write = sem_open/SEM_READ");
         exit(EXIT_FAILURE);
