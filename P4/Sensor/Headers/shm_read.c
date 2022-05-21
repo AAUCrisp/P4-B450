8c39a90a8708aff7f47e3336ef6a2195c6cdd140
ChineseWarlord <ChineseWarlord@github.com>
2022-05-21 20:00:01 +0200

    Changes

--- P4/Sensor/Headers/shm_read.c
+++ P4/Sensor/Headers/shm_read.c
@@ -28,15 +28,15 @@
 
 void* shm_read(const int SIZE, const char* name) {
     /* Semaphore variables */
-    sem_t* SEM_WRITE = sem_open(SEM_WRITE_FNAME, 0);
-    printf("shm_read SEM_WRITE: %s\n", (char*)SEM_WRITE);
+    sem_t* SEM_WRITE = sem_open(SEM_WRITE_FNAME, 1);
+    printf("shm_read SEM_WRITE: %s\n", SEM_WRITE);
     if (SEM_WRITE == SEM_FAILED) {
         perror("shm_read = sem_open/SEM_WRITE");
         exit(EXIT_FAILURE);
     }
 
-    sem_t* SEM_READ = sem_open(SEM_READ_FNAME, 0);
-    printf("shm_read SEM_READ: %s\n", (char*)SEM_READ);
+    sem_t* SEM_READ = sem_open(SEM_READ_FNAME, 1);
+    printf("shm_read SEM_READ: %s\n", SEM_READ);
     if (SEM_READ == SEM_FAILED) {
         perror("shm_read = sem_open/SEM_READ");
         exit(EXIT_FAILURE);
