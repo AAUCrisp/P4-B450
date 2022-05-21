11b6f57b984250ca5f97e2aba23943602d1b37d2
ChineseWarlord <ChineseWarlord@github.com>
2022-05-21 20:32:56 +0200

    C

--- P4/Sensor/Headers/shm_read.c
+++ P4/Sensor/Headers/shm_read.c
@@ -29,16 +29,18 @@
 void* shm_read(const int SIZE, const char* name) {
     printf("Do I reach here 1\n");
     /* Semaphore variables */
-    sem_t* SEM_WRITE = sem_init(SEM_WRITE_FNAME, 1);
+    sem_t* SEM_WRITE = sem_init(SEM_WRITE_FNAME, 1, 1);
     printf("WHY NO WORK? 1\n");
+    printf("SEM_WRITE Semaphore value: %d\n", int sem_getvalue(SEM_WRITE, int* valp));
     if (SEM_WRITE == SEM_FAILED) {
         perror("shm_read = sem_open/SEM_WRITE");
         exit(EXIT_FAILURE);
     }
     printf("Do I reach here 2\n");
 
-    sem_t* SEM_READ = sem_init(SEM_READ_FNAME, 1);
+    sem_t* SEM_READ = sem_init(SEM_READ_FNAME, 1, 1);
     printf("WHY NO WORK? 2\n");
+    printf("SEM_READ Semaphore value: %d\n", int sem_getvalue(SEM_READ, int* valp));
     if (SEM_READ == SEM_FAILED) {
         perror("shm_read = sem_open/SEM_READ");
         exit(EXIT_FAILURE);
@@ -51,7 +53,7 @@ void* shm_read(const int SIZE, const char* name) {
     /* pointer to shared memory object */
     void* ptr;
 
-    //sem_wait(SEM_READ);
+    sem_wait(SEM_READ);
     printf("Do I reach here 4\n");
     /* open the shared memory object */
     shm_fd = shm_open(name, O_RDONLY, 0666);
@@ -62,11 +64,11 @@ void* shm_read(const int SIZE, const char* name) {
     ptr = mmap(NULL, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
     printf("Read from shm_read: %s\n", (char*)ptr);
 
-    //sem_post(SEM_WRITE);
+    sem_post(SEM_WRITE);
     printf("Do I reach here 6\n");
-    //sem_close(SEM_READ);
+    sem_close(SEM_READ);
     printf("Do I reach here 7\n");
-    //sem_close(SEM_WRITE);
+    sem_close(SEM_WRITE);
     printf("Do I reach here 8\n");
 
     // printf("This is ptr memory map: %p\n", ptr);
