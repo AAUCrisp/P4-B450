23fa3efd26600f84fb9e8dc74940b46ca3a76f99
ChineseWarlord <ChineseWarlord@github.com>
2022-05-21 23:03:24 +0200

    Update shm_write.cpp

--- P4/Control_Unit/Headers/shm_write.cpp
+++ P4/Control_Unit/Headers/shm_write.cpp
@@ -38,7 +38,7 @@ void shm_write(const char* message, const int SIZE, const char* name) {
     /* write to the shared memory object */
     sprintf((char*)ptr, "%s", message);
     printf("SHM fd in WRITE: %d\n", shm_fd);
-    close(shm_fd);
+    //close(shm_fd);
     // printf("This is ptr: %p\n", ptr);
     // printf("This is ptr char*: %s\n", (char*)ptr);
     // printf("This is shm_fd: %d\n", shm_fd);
