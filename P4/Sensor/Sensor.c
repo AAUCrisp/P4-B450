8b70ad33af1dcd6b7ff7bfaaef498c78f11fe384
ChineseWarlord <ChineseWarlord@github.com>
2022-05-21 14:38:18 +0200

    Update Sensor.c

--- P4/Sensor/Sensor.c
+++ P4/Sensor/Sensor.c
@@ -16,6 +16,7 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <math.h>
 
 #include "Headers/SocketFunctions.h"
 #include "Headers/shm_write_read.h"
@@ -68,6 +69,7 @@ int main(int argc, char* argv[]) {
     /* Execution time variables */
     int iter = 1000;
     // struct timespec start, end;
+    double nan = (!isnan(dLat/dPhi)) ? dLat/dPhi : cos(lat1);
     double Execution_Time[iter];
     double Execution_Time2[iter];
     double Execution_Sum;
@@ -138,14 +140,8 @@ int main(int argc, char* argv[]) {
             Clock_End = clock();
             Execution_Time[i] += (double)(Clock_End - Clock_Start) / CLOCKS_PER_SEC;
 
-            /*if (Execution_Time[i] <= 0.000200 && Execution_Time[i] >= 0.000300){
+            if (Execution_Time[i] != isnan(Execution_Time[i])) {
                 Execution_Sum += Execution_Time[i];
-            }*/
-            if (i == 900) {
-
-                Execution_Sum += Execution_Time2[i];
-                printf("Execution_Time[%d]: %f\n", i, Execution_Time2[i]);
-                printf("Execution_Sum = %f\n", Execution_Sum2);
             }
 
             printf("Execution_Time[%d]: %f\n", i, Execution_Time[i]);
