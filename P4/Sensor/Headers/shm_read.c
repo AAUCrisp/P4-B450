190eeafcb4b47c775098432066b5cafe71061b23
ChineseWarlord <ChineseWarlord@github.com>
2022-05-21 20:21:52 +0200

    C

--- P4/Sensor/Headers/shm_read.c
+++ P4/Sensor/Headers/shm_read.c
@@ -29,20 +29,20 @@
 void* shm_read(const int SIZE, const char* name) {
     printf("Do I reach here 1\n");
     /* Semaphore variables */
-    //sem_t* SEM_WRITE = sem_open(SEM_WRITE_FNAME, 1);
+    sem_t* SEM_WRITE = sem_init(SEM_WRITE_FNAME, 1);
     printf("WHY NO WORK? 1\n");
-    /*if (SEM_WRITE == SEM_FAILED) {
+    if (SEM_WRITE == SEM_FAILED) {
         perror("shm_read = sem_open/SEM_WRITE");
         exit(EXIT_FAILURE);
-    }*/
+    }
     printf("Do I reach here 2\n");
 
-    //sem_t* SEM_READ = sem_open(SEM_READ_FNAME, 1);
+    sem_t* SEM_READ = sem_init(SEM_READ_FNAME, 1);
     printf("WHY NO WORK? 2\n");
-    /*if (SEM_READ == SEM_FAILED) {
+    if (SEM_READ == SEM_FAILED) {
         perror("shm_read = sem_open/SEM_READ");
         exit(EXIT_FAILURE);
-    }*/
+    }
     printf("Do I reach here 3\n");
 
     /* shared memory file descriptor */
