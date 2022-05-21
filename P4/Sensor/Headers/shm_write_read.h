ae1b217f9bbd595c9a49288c8d6cfd96fb77c14d
ChineseWarlord <ChineseWarlord@github.com>
2022-05-21 20:40:48 +0200

    Update shm_write_read.h

--- P4/Sensor/Headers/shm_write_read.h
+++ P4/Sensor/Headers/shm_write_read.h
@@ -2,8 +2,8 @@
 #define SHM_WRITE_READ_H
 
 /* Semaphore Names */
-#define SEM_READ_FNAME "/SEM_READ"
-#define SEM_WRITE_FNAME "/SEM_WRITE"
+sem_t SEM_READ_FNAME "/SEM_READ"
+sem_t SEM_WRITE_FNAME "/SEM_WRITE"
 
 void shm_write(const char* message, const int SIZE, const char* name);
 
