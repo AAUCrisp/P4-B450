a145b0ff80b51965a5501a140adb78c0e86cd0e7
ChineseWarlord <ChineseWarlord@github.com>
2022-05-22 14:59:41 +0200

    c

--- P4/Sensor/Sensor.c
+++ P4/Sensor/Sensor.c
@@ -228,7 +228,7 @@ int main(int argc, char* argv[]) {
         long hours = ((((((long)(timestamp / 1000) - milliseconds) / 1000) - seconds) / 60) - minutes) / 60;
 
         Execution_Average = Execution_Sum / iter;
-        printf("Execution average: %Lf ms\n", Execution_Average);
+        printf("\n\nExecution average: %Lf ms\n", Execution_Average);
         printf("Total time: %ld\n", (Time_Ended - Time_Started));
         printf("Total_Time_Elapsed [HH:MM:SS:MS]: %ld:%ld:%ld:%ld\n", hours, minutes, seconds, milliseconds);
         printf("Total failed counts: %d\n", fail_count);
