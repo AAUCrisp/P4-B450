c427583ab5de56e18220c6d096ad17834bebc074
ChineseWarlord <ChineseWarlord@github.com>
2022-05-22 14:55:47 +0200

    c

--- P4/Sensor/Sensor.c
+++ P4/Sensor/Sensor.c
@@ -168,22 +168,40 @@ int main(int argc, char* argv[]) {
 
             seconds = end.tv_sec - begin.tv_sec;
             nanoseconds = end.tv_nsec - begin.tv_nsec;
-           
-            elapsed = seconds + nanoseconds * 1e-9;
-            printf("\n\nelapsed time: %f\n", elapsed);
-            //sleep(5);
-            Execution_Time[i] = elapsed;
-            printf("Execution_Time[%d]: %Lf\n", i, Execution_Time[i]);
-            //sleep(5);
-            Execution_Sum += elapsed;
-            printf("Execution_Sum: %Lf\n", Execution_Sum);
-            //sleep(5);
 
+            elapsed = seconds + nanoseconds * 1e-9;
+            if (elapsed > 10000) {
+                fail_count++;
+                printf("fail count: %d\n", fail_count);
+                sleep(5);
+            } else {
+                printf("\n\nelapsed time: %f\n", elapsed);
+            }
+            // sleep(5);
+            Execution_Time[i] = elapsed;
+            if (Execution_Time[i] > 10000) {
+                fail_count++;
+                printf("fail count: %d\n", fail_count);
+                sleep(5);
+            } else {
+                printf("Execution_Time[%d]: %Lf\n", i, Execution_Time[i]);
+            }
+            // sleep(5);
+            Execution_Sum += elapsed;
+            if (Execution_Sum > 10000){
+                fail_count++;
+                printf("fail count: %d\n", fail_count);
+                sleep(5);
+            } else {
+            printf("Execution_Sum: %Lf\n", Execution_Sum);
+
+            }
+            // sleep(5);
 
             // Execution_Time[i] += (double)(Clock_End - Clock_Start) / CLOCKS_PER_SEC;
-            //Execution_Temp += (long double)(Clock_End - Clock_Start) / CLOCKS_PER_SEC;
+            // Execution_Temp += (long double)(Clock_End - Clock_Start) / CLOCKS_PER_SEC;
 
-            //printf("Execution_Temp: %Lf\n", Execution_Temp);
+            // printf("Execution_Temp: %Lf\n", Execution_Temp);
             /*
             if (Execution_Temp > 1000000.000 || Execution_Temp < -1000000.000) {
                 fail_count++;
