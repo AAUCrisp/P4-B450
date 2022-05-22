ba85347bfb11659b54423005717b90a8134d050b
ChineseWarlord <ChineseWarlord@github.com>
2022-05-22 18:59:21 +0200

    c

--- P4/Sensor/Headers/shm_write.c
+++ P4/Sensor/Headers/shm_write.c
@@ -28,23 +28,21 @@
 extern int errno;
 
 void shm_write(const char* message, const int SIZE, const char* name) {
-
     /* Semaphore variables */
-    //sem_unlink(SEM_READ);
-    //sem_unlink(SEM_WRITE);
+    // sem_unlink(SEM_READ);
+    // sem_unlink(SEM_WRITE);
 
     int sem_write = sem_init(&SEM_WRITE, 1, 1);
     if (sem_write == -1) {
         perror("shm_write = sem_open/SEM_WRITE");
         exit(EXIT_FAILURE);
     }
-   
+
     int sem_read = sem_init(&SEM_READ, 1, 1);
     if (sem_read == -1) {
         perror("shm_write = sem_open/SEM_READ");
         exit(EXIT_FAILURE);
     }
-
 
     /* shared memory file descriptor */
     int shm_fd;
@@ -65,13 +63,11 @@ void shm_write(const char* message, const int SIZE, const char* name) {
 
     /* configure the size of the shared memory object */
     ftruncate(shm_fd, SIZE);
-    if (sem_wait(&SEM_WRITE) == -1) {
-        perror("SEM_WRITE sem_wait failed");
-    }
 
-    if (sem_wait(&SEM_WRITE) == -1) {
-        perror("SEM_READ sem_wait failed");
-    }
+    /*
+        if (sem_wait(&SEM_WRITE) == -1) {
+            perror("SEM_READ sem_wait failed");
+        }*/
     /* memory map the shared memory object */
     ptr = mmap(NULL, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if (ptr == MAP_FAILED) {
@@ -82,18 +78,17 @@ void shm_write(const char* message, const int SIZE, const char* name) {
     sprintf(ptr, "%s", message);
     printf("Wrote from shm_write: %s\n", (char*)ptr);
 
-    //munmap(ptr, SIZE);
+    // munmap(ptr, SIZE);
     printf("shm_fd value: %d\n", shm_fd);
     close(shm_fd);
 
-    if (sem_post(&SEM_READ)) {
+    /*if (sem_post(&SEM_READ)) {
         perror("SEM_WRITE sem_post failed");
-    }
+    }*/
 
-    //sem_close(&SEM_READ);
+    // sem_close(&SEM_READ);
 
-    //sem_close(&SEM_WRITE);
-
+    // sem_close(&SEM_WRITE);
 
     // printf("This is ptr: %p\n", ptr);
     // printf("This is ptr char*: %s\n", (char*)ptr);
