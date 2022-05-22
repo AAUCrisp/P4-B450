e4d7cb8332992f65f8ced08884943bb579dea85f
ChineseWarlord <ChineseWarlord@github.com>
2022-05-22 19:32:16 +0200

    c

--- P4/Actuator/Actuator.cpp
+++ P4/Actuator/Actuator.cpp
@@ -55,7 +55,7 @@ int main() {
     /* Execution time variables */
     int count = 0;
     int fail_count = 0;
-    int iter = 500000;
+    static int iter = 500000;
     long double Execution_Time[iter];
     long double Execution_Temp = 0;
     long double Execution_Sum = 0;
