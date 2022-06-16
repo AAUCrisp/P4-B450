3cd0357dbc889f38a5fe9c06f1d0cbe50e7f4112
AAUCrisp <70756477+AAUCrisp@users.noreply.github.com>
2022-06-16 14:13:09 +0200

    Cleanup
    All the old shit is in the "Full-Semester" branch now

--- P4/Test/Other_Test_Files/Process_Creation/shm_write_read.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHM_WRITE_READ_H
-#define SHM_WRITE_READ_H
-
-void shm_write(void* message, const int SIZE, const char* name);
-
-char* shm_read(const int SIZE, const char* name);
-
-#endif
--- P4/Test/Steffen/Headers/combined.cpp
+++
@@ -1,4 +0,0 @@
-extern "C" {
-    #include "Control_Unit.cpp"
-
-}
