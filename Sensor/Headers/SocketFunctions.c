1fa45296da65202391fb5b754b1d538fcb79ee14
ChineseWarlord <ChineseWarlord@github.com>
2022-06-21 16:22:08 +0200

    shit

--- Sensor/Headers/SocketFunctions.c
+++ Sensor/Headers/SocketFunctions.c
@@ -1,6 +1,3 @@
-
-#include "SocketFunctions.h"
-
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -20,6 +17,7 @@
 #include <time.h>
 #include <unistd.h>
 
+#include "SocketFunctions.h"
 #include "shm_read_write.h"
 
 /* Troubleshooting Options */
