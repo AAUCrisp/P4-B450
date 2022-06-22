b791b4a8a858552dba02538d6400838cb3f9e95f
ChineseWarlord <ChineseWarlord@github.com>
2022-06-22 16:42:16 +0200

    test

--- Control_Unit/Headers/shm_write_read.h
+++ Control_Unit/Headers/shm_write_read.h
@@ -1,13 +1,13 @@
 #ifndef SHM_READ_HEADER
 #define SHM_READ_HEADER
 
-// char* shm_read(const int SIZE, const char* name);
+char* shm_read(const int SIZE, const char* name);
 
 #endif
 
 #ifndef SHM_WRITE_HEADER
 #define SHM_WRITE_HEADER
 
-// void shm_write(void* message, const int SIZE, const char* name);
+void shm_write(void* message, const int SIZE, const char* name);
 
 #endif
