10e00a76906e41331605523b289139d2b9b4a4c2
ChineseWarlord <simonbaks@live.dk>
2022-06-23 21:48:51 +0200

    test

--- Actuator/Headers/SocketFunctions.cpp
+++ Actuator/Headers/SocketFunctions.cpp
@@ -205,7 +205,8 @@ void *receiveLTE(void *socket) {
         // fputs(" LTE\n\n", fp1);
 
         // fprintf(fp1, "Received at: %s\n %s    LTE : ", curr_time, message);
-        fprintf(fp1, "%s %s %s %s", "Received at: ", curr_time, "\n", message);
+       
+        fprintf(fp1, "%s %s \n%s %s", "Received at: ", curr_time, message, " LTE");
 
         fclose(fp1);
         // File.close();
@@ -243,7 +244,7 @@ void *receiveWiFi(void *socket) {
         // fputs(message, fp2);
         // fputs(" WiFi\n\n", fp2);
 
-        fprintf(fp2, "%s %s %s %s", "Received at: ", curr_time, "\n", message);
+        fprintf(fp2, "%s %s \n%s %s", "Received at: ", curr_time, message, " WiFi");
 
         fclose(fp2);
         // File.close();
