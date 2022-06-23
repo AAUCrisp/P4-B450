fe4d7edb105da3ab1e1a297e0790bfa4d7adc83c
ChineseWarlord <ChineseWarlord@github.com>
2022-06-23 13:38:36 +0200

    Returned original IPs

--- Sensor/Sensor.c
+++ Sensor/Sensor.c
@@ -48,19 +48,19 @@ int main(int argc, char* argv[]) {
     /* Initialize PORT & INTERFACE*/
     uint PORT_LTE_TRANSMITTER = 9000;
     uint PORT_WiFi_TRANSMITTER = 9001;
-    // const char* LTE = "wwan0";
-    // const char* WiFi = "wlan0";
-    // const char* IP_LTE = "10.20.0.16";  // Default: IP of Control Unit
+    const char* LTE = "wwan0";
+    const char* WiFi = "wlan0";
+    const char* IP_LTE = "10.20.0.16";  // Default: IP of Control Unit
     // const char* IP_LTE = "10.20.0.13";      // IP of Actuator
     // const char* IP_LTE = "10.20.0.10";      // IP of Sensor
-    // const char* IP_WiFi = "192.168.1.136";  // Default: IP of Control Unit
+    const char* IP_WiFi = "192.168.1.136";  // Default: IP of Control Unit
     // const char* IP_WiFi = "192.168.1.143";  // IP of Actuator
     // const char* IP_WiFi = "192.168.1.160";  // IP of Sensor
 
-    const char* LTE = "lo";             // Test loopback
-    const char* WiFi = "lo";            // Test loopback
-    const char* IP_LTE = "127.0.0.1";   // Test loopback
-    const char* IP_WiFi = "127.0.0.1";  // Test loopback
+    // const char* LTE = "lo";             // Test loopback
+    // const char* WiFi = "lo";            // Test loopback
+    // const char* IP_LTE = "127.0.0.1";   // Test loopback
+    // const char* IP_WiFi = "127.0.0.1";  // Test loopback
 
     /* misc */
     pthread_t T1;
