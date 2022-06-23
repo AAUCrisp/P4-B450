8a3f43e2c8b58dae3bfc58392159821599ca2e4d
ChineseWarlord <ChineseWarlord@github.com>
2022-06-23 15:50:25 +0200

    test

--- Control_Unit/ControlUnit.cpp
+++ Control_Unit/ControlUnit.cpp
@@ -74,7 +74,7 @@ int main(int argc, char *argv[]) {
     /* Start signal monitoring process */
     pid_t signal_monitor;     // Prepare the process ID for monitoring
     if(monitor == 1) {
-        signal_monitor = fork();  // Starts new process
+        //signal_monitor = fork();  // Starts new process
     }
     if (signal_monitor == 0) {
         if(troubleshooting_print == 1) {
@@ -84,7 +84,7 @@ int main(int argc, char *argv[]) {
 
         char* path = (char*) "./SignalMonitoring";                 // Path of the file for new process to run
         char* args[] = { (char*)"./SignalMonitoring&", GSV_arg_used, GSV_print, GSV_sleep, GSV_sleep_arg, GSV_tech, GSV_tech_arg, (char*) NULL};       // Command for the function to execute, always ended on NULL argument
-        execv(path, args);                                  // Tells the new process to "reset" and run a different code instead
+        //execv(path, args);                                  // Tells the new process to "reset" and run a different code instead
         printf("  ERROR: DIDN'T START THE MONITORING PROCESS!!\n");  // Should never get this far!
     } 
 
