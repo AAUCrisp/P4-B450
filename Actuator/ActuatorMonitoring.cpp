48bc86f24e2362ce64fc9626cbf7e3da854e04be
ChineseWarlord <simonbaks@live.dk>
2022-06-23 20:29:16 +0200

    test

--- Actuator/ActuatorMonitoring.cpp
+++ Actuator/ActuatorMonitoring.cpp
@@ -27,8 +27,10 @@ int main() {
     /* Initialize PORT & INTERFACE*/
     uint PORT_LTE_RECEIVER = 9004;
     uint PORT_WiFi_RECEIVER = 9005;
-    const char* LTE = "wwan0";
-    const char* WiFi = "wlan0";
+    //const char* LTE = "wwan0";
+    //const char* WiFi = "wlan0";
+    const char* LTE = "lo";
+    const char* WiFi = "lo";
 
     /* Misc */
     pthread_t T1;
