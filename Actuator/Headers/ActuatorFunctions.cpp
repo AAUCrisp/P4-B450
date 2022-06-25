a7bc80dedee28c967177ac3eb380bb5d8e477026
ChineseWarlord <simonbaks@live.dk>
2022-06-25 17:33:37 +0200

    test

--- Actuator/Headers/ActuatorFunctions.cpp
+++ Actuator/Headers/ActuatorFunctions.cpp
@@ -91,7 +91,7 @@ char *Timestamp2() {
     gettimeofday(&tv2, NULL);
     timeinfo2 = localtime(&tv2.tv_sec);
 
-    sprintf(curr_time2, "[%d:%d:%d.%03ld %d/%d/%d]", timeinfo2->tm_hour, timeinfo2->tm_min, timeinfo2->tm_sec, tv.tv_usec / 1000, timeinfo2->tm_mday, timeinfo2->tm_mon + 1, timeinfo2->tm_year + 1900);
+    sprintf(curr_time2, "[%d:%d:%d.%03ld %d/%d/%d]", timeinfo2->tm_hour, timeinfo2->tm_min, timeinfo2->tm_sec, tv2.tv_usec / 1000, timeinfo2->tm_mday, timeinfo2->tm_mon + 1, timeinfo2->tm_year + 1900);
 
     return curr_time2;
 }
