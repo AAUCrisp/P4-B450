5ffc23874c5864d9a309cb1fc16dbd6fcd048444
ChineseWarlord <simonbaks@live.dk>
2022-06-25 14:15:26 +0200

    test

--- Actuator/Headers/SocketFunctions.cpp
+++ Actuator/Headers/SocketFunctions.cpp
@@ -196,7 +196,7 @@ void *receiveLTE(void *socket) {
     printf("maxshit: %d\n", maxshit);
 
     struct timeval tv;
-    tv.tv_sec = 0;
+    tv.tv_sec = 10;
     tv.tv_usec = 0;
 
     while (1) {
@@ -206,15 +206,7 @@ void *receiveLTE(void *socket) {
 
         if (nready > 0) {
             printf("select value: %d\n", nready);
-            
-        }
-        if (nready == 0) {
-            STOP = 0;
-            sprintf(stopshit, "%d", STOP);
-            printf("select value: %d\n", nready);
-        }
-        if (FD_ISSET(maxshit, &readfds)) {
-            FD_CLR(maxshit, &readfds);
+
             fp1 = fopen("Logs/log.txt", "a+");
             // printf("receiveLTE socket: %d\n", sock->sockLTE_RECEIVER);
             RX_LTE = recvfrom(sock->sockLTE_RECEIVER, message, BUFFER, 0, (struct sockaddr *)&sock->ServerLTE_RECEIVER, &LenLTE);
@@ -231,6 +223,11 @@ void *receiveLTE(void *socket) {
             sprintf(writer, "%s", message);
             fprintf(fp1, "%s %s %s\n", message, curr_time, "LTE");
             fclose(fp1);
+        }
+        if (nready == 0) {
+            STOP = 0;
+            sprintf(stopshit, "%d", STOP);
+            printf("select value: %d\n", nready);
         }
     }
 }
