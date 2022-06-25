fee3aa3a6c767a549fc8de8fbeef1cd9377b0ef1
ChineseWarlord <simonbaks@live.dk>
2022-06-25 20:26:57 +0200

    test

--- Actuator/Headers/SocketFunctions.cpp
+++ Actuator/Headers/SocketFunctions.cpp
@@ -249,12 +249,12 @@ void *receiveLTE(void *socket) {
         sock->packet_count_LTE++;
     }
     sleep(1);
-    cout << "\n\n=====================================================\n"
+    cout << "\n\n====================================================="
          << endl;
     cout << "\nLTE : Execution_Sum: " << sock->Execution_Sum_LTE << endl;
     printf("LTE : Total failed counts: %d\n", sock->fail_count_LTE);
     printf("LTE : Total packets received via LTE: %d\n\n", sock->packet_count_LTE);
-    cout << "\n=====================================================\n\n"
+    cout << "=====================================================\n\n"
          << endl;
     sleep(10);
     return 0;
@@ -321,12 +321,12 @@ void *receiveWiFi(void *socket) {
         sock->packet_count_WiFi++;
     }
     sleep(1);
-    cout << "\n\n=====================================================\n"
+    cout << "\n\n====================================================="
          << endl;
     cout << "\nWiFi : Execution_Sum: " << sock->Execution_Sum_WiFi << endl;
     printf("WiFi : Total failed counts: %d\n", sock->fail_count_WiFi);
     printf("WiFi : Total packets received via WiFi: %d\n\n", sock->packet_count_WiFi);
-    cout << "\n=====================================================\n\n"
+    cout << "=====================================================\n\n"
          << endl;
     sleep(10);
     return 0;
