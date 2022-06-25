9d236783389a1d329525ddf73f2912a06489eb6a
ChineseWarlord <simonbaks@live.dk>
2022-06-25 19:54:30 +0200

    test

--- Control_Unit/ControlUnit.cpp
+++ Control_Unit/ControlUnit.cpp
@@ -51,7 +51,7 @@ int main(int argc, char* argv[]) {
     //  const char* Actuator_IP_WiFi = "192.168.1.160";     // Sensor IP
 
     const char* Actuator_IP_LTE = "127.0.0.1";   // test loopback
-    const char* Actuator_IP_WiFi = "";  // test loopback
+    const char* Actuator_IP_WiFi = "192.168.0.134";  // test loopback
     const char* LTE = "lo";                      // test loopback
     const char* WiFi = "enp0s3";                     // test loopback
 
