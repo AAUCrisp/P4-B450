dce60f8a4ee835d14d1a12ad79d8eb22b1814b22
ChineseWarlord <simonbaks@live.dk>
2022-06-26 18:09:24 +0200

    test

--- Control_Unit/Headers/Sockets.cpp
+++ Control_Unit/Headers/Sockets.cpp
@@ -437,8 +437,8 @@ void *transmit_command_WiFi(void *socket, char *message) {
  void *transmit_command(void *socket, char *message, int gsv) {
 //void *transmit_command(int *socket, char *message, int gsv) {
     //Sockets *sock = (Sockets *)socket;
-    printf("\n\nINSIDE Actuator Socket WiFi: %d\n", sock->act_WiFi);
-    printf("INSIDE Actuator Socket LTE: %d\n", sock->act_LTE);
+    printf("\n\nINSIDE Actuator Socket WiFi: %d\n", socket->act_WiFi);
+    printf("INSIDE Actuator Socket LTE: %d\n", socket->act_LTE);
 
     if (print_act_out == 1) {
         cout << "  ======================\n  ==== SEND COMMAND ====\n  ======= entry ========\n"
