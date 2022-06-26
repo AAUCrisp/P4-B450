52e3d4ce820b5124968ebe56fb871bb8157887fa
ChineseWarlord <simonbaks@live.dk>
2022-06-27 00:07:23 +0200

    test

--- Actuator/Headers/SocketFunctions.cpp
+++ Actuator/Headers/SocketFunctions.cpp
@@ -212,9 +212,8 @@ void *receiveLTE(void *socket) {
         printf("RX_WiFi: %d\n", RX_WiFi);
         printf("STOP_LTE: %d\n", sock->STOP_LTE);
         printf("STOP_WiFi: %d\n", sock->STOP_WiFi);
+
         if (RX_LTE == -1) {
-            //sock->STOP_WiFi = 1;
-            // if(sock->STOP_LTE == 2 && sock->STOP_WiFi ==1){return 0;}
             return 0;
         } else {
             sock->STOP_LTE = 0;
@@ -282,6 +281,7 @@ void *receiveWiFi(void *socket) {
         printf("RX_LTE: %d\n", RX_LTE);
         printf("STOP_WiFi: %d\n", sock->STOP_WiFi);
         printf("STOP_LTE: %d\n", sock->STOP_LTE);
+        
         if (RX_WiFi == -1) {
             while (1) {
                 if (RX_WiFi == -1 && RX_LTE == -1) {
