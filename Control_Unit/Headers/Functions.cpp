8ffa6f8ac3448605ad92b9eab2fa503dad55785c
ChineseWarlord <simonbaks@live.dk>
2022-06-26 23:43:42 +0200

    test

--- Control_Unit/Headers/Functions.cpp
+++ Control_Unit/Headers/Functions.cpp
@@ -66,13 +66,10 @@ void WiFi_command(Sockets sock) {
         printf("WiFi - localRX_LTE: %d\n", localRX_LTE);
         if (sock.RX_WiFi == -1) {
             while(1) {
-                if (sock.RX_WiFi == -1 && sock.RX_LTE == -1){
+                if (sock.RX_WiFi == -1 && localRX_LTE == -1){
                     break;
                     sock.STOP_WiFi = 1;
                 }
-                sleep(2);
-                printf("WiFi - localRX_LTE: %d\n", localRX_LTE);
-                printf("WiFi - AM I STUCK HERE?\n");
             }
         } else {
             sock.STOP_WiFi = 0;
