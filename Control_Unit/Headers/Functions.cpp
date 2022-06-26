9f450156f4d65bc6c083869dd5957a0ffb9ae398
ChineseWarlord <simonbaks@live.dk>
2022-06-26 15:23:05 +0200

    test

--- Control_Unit/Headers/Functions.cpp
+++ Control_Unit/Headers/Functions.cpp
@@ -97,7 +97,7 @@ void WiFi_command(Sockets sock) {
 }
 
 void* LTE_command(void* socket) {
-    
+    Sockets* sock = (Sockets*)socket;
     void* message;
     char msgDump[32];
     char tempMsg[32];
@@ -172,8 +172,8 @@ void* LTE_command(void* socket) {
         int gsv = atoi(GSV_read);  // Convert to integer
                                    // printf("converted GSV: %s\n", (char*)GSV_read);
         if (strcmp(tempMsg, coordinate.c_str()) != 0) {
-            Sockets* sock = (Sockets*)socket;
             transmit_command(&sock, LTEmsg, gsv);
+            printf("socket: %s\n", (char*)&sock);
             strcpy(tempMsg, coordinate.c_str());
             char* timeLTE = Timestamp();
 
