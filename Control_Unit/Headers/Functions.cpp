84b59527abb34e72a6e1ac225206ec03da74e131
ChineseWarlord <simonbaks@live.dk>
2022-06-26 23:43:52 +0200

    test

--- Control_Unit/Headers/Functions.cpp
+++ Control_Unit/Headers/Functions.cpp
@@ -63,7 +63,6 @@ void WiFi_command(Sockets sock) {
         printf("WiFi - RX_WiFi: %d\n", sock.RX_WiFi);
         printf("WiFi - STOP_LTE: %d\n", sock.STOP_LTE);
         printf("WiFi - STOP_WiFi: %d\n", sock.STOP_WiFi);
-        printf("WiFi - localRX_LTE: %d\n", localRX_LTE);
         if (sock.RX_WiFi == -1) {
             while(1) {
                 if (sock.RX_WiFi == -1 && localRX_LTE == -1){
