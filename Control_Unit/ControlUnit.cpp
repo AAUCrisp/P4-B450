1ef44ffa51782c364c6646311b9b82753623b2d2
ChineseWarlord <simonbaks@live.dk>
2022-06-28 00:40:23 +0200

    test :horse:

--- Control_Unit/ControlUnit.cpp
+++ Control_Unit/ControlUnit.cpp
@@ -48,7 +48,7 @@ int main(int argc, char* argv[]) {
     // const char* Actuator_IP_WiFi = "10.42.0.118";    // Default: Actuator IP (AP)
     // const char* Actuator_IP_LTE = "10.20.0.16";      // Control IP
     // const char* Actuator_IP_LTE = "10.20.0.10";      // Sensor IP
-    // const char* Actuator_IP_WiFi = "192.168.1.143";  //Default: Actuator IP
+    // const char* Actuator_IP_WiFi = "192.168.1.143";  // Default: Actuator IP
     // const char* Actuator_IP_WiFi = "192.168.1.136";  // Control IP
     // const char* Actuator_IP_WiFi = "192.168.1.160";  // Sensor IP
 
@@ -71,7 +71,7 @@ int main(int argc, char* argv[]) {
     Sockets sock;
     Sockets_Receiver(&sock, PORT_LTE_RECEIVER, PORT_WiFi_RECEIVER, LTE, WiFi);
     Sockets_Actuator(&sock, Actuator_IP_LTE, Actuator_IP_WiFi, PORT_LTE_ACTUATOR, PORT_WiFi_ACTUATOR, LTE, WiFi);
-    
+
     /* Execution time variables */
     struct timespec begin_program, end_program;
     unsigned long seconds = 0;
