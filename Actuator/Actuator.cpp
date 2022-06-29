0b326c83c4a8ff388f568750b0d4028eae1076ec
ChineseWarlord <ChineseWarlord@github.com>
2022-06-29 16:15:46 +0200

    test

--- Actuator/Actuator.cpp
+++ Actuator/Actuator.cpp
@@ -86,8 +86,8 @@ int main() {
     unsigned long seconds = 0;
     unsigned long nanoseconds = 0;
     long double Execution_Average = 0;
-    //long double Execution_AverageLTE = 0;
-    //long double Execution_AverageWiFi = 0;
+    long double Execution_AverageLTE = 0;
+    long double Execution_AverageWiFi = 0;
 
     sock.Execution_Sum_WiFi = 0;
     sock.Execution_Sum_LTE = 0;
