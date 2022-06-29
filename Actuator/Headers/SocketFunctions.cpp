6e7f0a18097437a55f84303358ea2d47bc04f184
ChineseWarlord <ChineseWarlord@github.com>
2022-06-29 13:59:31 +0200

    test

--- Actuator/Headers/SocketFunctions.cpp
+++ Actuator/Headers/SocketFunctions.cpp
@@ -226,7 +226,7 @@ void *receiveLTE(void *socket) {
         }
         Timestamp();
         sock->packet_count_LTE++;
-        printf("\rtotal packets LTE: %d\n", sock->packet_count_LTE);
+        printf("\rtotal packets LTE: %d", sock->packet_count_LTE);
         fflush(stdout);
         fp1 = fopen("Logs/log.txt", "a+");
         fprintf(fp1, "%s %s %s\n", message, curr_time, "LTE");
@@ -302,7 +302,7 @@ void *receiveWiFi(void *socket) {
         }
         Timestamp();
         sock->packet_count_WiFi++;
-        printf("\rtotal packets WiFi: %d\n", sock->packet_count_WiFi);
+        printf("\rtotal packets WiFi: %d", sock->packet_count_WiFi);
         fflush(stdout);
         fp2 = fopen("Logs/log.txt", "a+");
         fprintf(fp2, "%s %s %s\n", message, curr_time, "WiFi");
